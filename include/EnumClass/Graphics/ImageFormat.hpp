25a5eea5e20f21a6e4632ce5953749b137f76146
zawa-ch <lunatic.zawa.ch@gmail.com>
2019-10-05 21:48:26 +0900

    added Graphics::ImageFormat

---
+++ include/EnumClass/Graphics/ImageFormat.hpp
@@ -0,0 +1,22 @@
+#ifndef __StationaryOrbit_Graphics_ImageFormat__
+#define __StationaryOrbit_Graphics_ImageFormat__
+namespace StationaryOrbit
+{
+namespace Graphics
+{
+
+	///	イメージの各チャネルの割当を定めるフォーマット情報。
+	enum class ImageFormat
+	{
+		Binary,	///< 二値。
+		Index8,	///< 8ビット インデックス。
+		Tone8,	///< 8ビット。
+		Tone16,	///< 16ビット。
+		ColorF16,	///< 16ビット浮動小数点数。
+		ColorF32,	///< 32ビット浮動小数点数。
+		ColorF64,	///< 64ビット浮動小数点数。
+	};
+
+}
+}
+#endif // __StationaryOrbit_Graphics_ImageFormat__
