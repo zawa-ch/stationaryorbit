4e425791930667075c79d9776ebd0160a65c7a69
zawa-ch <lunatic.zawa.ch@gmail.com>
2019-10-06 20:35:11 +0900

    edited Graphics::ImageInfomation

--- include/DataClass/Graphics/ImageInfomation.hpp
+++ include/DataClass/Graphics/ImageInfomation.hpp
@@ -9,6 +9,7 @@ namespace StationaryOrbit
 namespace Graphics
 {
 
+	///	画像が持つ情報を表します。
 	class ImageInfomation
 		: virtual public IImageInfomation
 	{
@@ -24,16 +25,22 @@ namespace Graphics
 
 		virtual ~ImageInfomation() = default;
 
+		///	画像の大きさを示すPointを取得します。
 		Point getSize() const;
 
+		///	画像の大きさを示すPointを設定します。
 		void setSize(const Point& value);
 
+		///	画像のチャネル数を取得します。
 		size_t getChannels() const;
 
+		///	画像のチャネル数を設定します。
 		void setChannels(const size_t& value);
 
+		///	画像の色の表現に使用されるフォーマットを取得します。
 		ImageFormat getFormat() const;
 
+		///	画像の色の表現に使用されるフォーマットを設定します。
 		void setFormat(const ImageFormat& value);
 
 	};
