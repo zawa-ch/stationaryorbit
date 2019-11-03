4b66b78b590f9fc0f540cd3d6d9b58abdf4164d1
zawa-ch <lunatic.zawa.ch@gmail.com>
2019-11-03 19:23:28 +0900

    edited NumericAnalysis::PolynomialFunction

--- include/NumericAnalysis/Logic/PolynomialFunction.hpp
+++ include/NumericAnalysis/Logic/PolynomialFunction.hpp
@@ -6,43 +6,31 @@
 namespace StationaryOrbit::NumericAnalysis
 {
 
-	///	分子と分母を持つ実数の多項式を表します。
+	///	実数の多項式を表します。
 	class PolynomialFunction
 		: public IMathmaticFunction<double>
 	{
 	private:
 
 		std::vector<double> num;
-		std::vector<double> denom;
 
 	public:
 
 		///	空のオブジェクトを作成します。
 		PolynomialFunction() = default;
 
-		///	分子の項を指定して多項式のオブジェクトを初期化します。
+		///	多項式のオブジェクトを初期化します。
 		///	x^n * num[n-1] + ... x^2 * num[2] + x * num[1] + num[0] の順で計算されます。
-		explicit PolynomialFunction(const std::vector<double>& numerator);
+		explicit PolynomialFunction(const std::vector<double>& value);
 
-		///	分子・分母の各項を指定して多項式のオブジェクトを初期化します。
-		///	(x^n * num[n-1] + ... x * num[1] + num[0])/(x^n * denom[n-1] + ... x * denom[1] + denom[0]) の順で計算されます。
-		PolynomialFunction(const std::vector<double>& numerator, const std::vector<double>& denominator);
+		///	多項式への参照を取得します。
+		std::vector<double>& Polynomial() { return num; }
 
-		///	多項式の分子部分への参照を取得します。
-		std::vector<double>& Numerator() { return num; }
-
-		///	多項式の分子部分を取得します。
-		std::vector<double> getNumerator() const { return num; }
-
-		///	多項式の分母部分への参照を取得します。
-		std::vector<double>& Denominator() { return denom; }
-
-		///	多項式の分母部分を取得します。
-		std::vector<double> getDenominator() const { return denom; }
+		///	多項式を取得します。
+		std::vector<double> getPolynomial() const { return num; }
 
 		///	この多項式に値を代入し、値を計算します。
 		double Calc(const double& value) const;
-
 		double operator()(const double& value) const { return Calc(value); }
 
 	};
--- src/NumericAnalysis/PolynomialFunction.cpp
+++ src/NumericAnalysis/PolynomialFunction.cpp
@@ -1,30 +1,19 @@
 #include "NumericAnalysis/Logic/PolynomialFunction.hpp"
 #include "NumericAnalysis/Structure/CompensatedFloat.hpp"
 
-StationaryOrbit::NumericAnalysis::PolynomialFunction::PolynomialFunction(const std::vector<double>& numerator)
-	: PolynomialFunction(numerator, std::vector<double> {1})
-{}
-
-StationaryOrbit::NumericAnalysis::PolynomialFunction::PolynomialFunction(const std::vector<double>& numerator, const std::vector<double>& denominator)
-	: num(numerator), denom(denominator)
+StationaryOrbit::NumericAnalysis::PolynomialFunction::PolynomialFunction(const std::vector<double>& value)
+	: num(value)
 {}
 
 double StationaryOrbit::NumericAnalysis::PolynomialFunction::Calc(const double& value) const
 {
-	if (num.empty() || denom.empty()) throw InvalidOperationException("");
+	if (num.empty()) throw InvalidOperationException("");
 	CompensatedDouble numvalue = CompensatedDouble();
-	CompensatedDouble denvalue = CompensatedDouble();
 	for(size_t i = num.size() - 1; 0 < i; i--)
 	{
 		numvalue += num[i];
 		numvalue *= value;
 	}
 	numvalue += num[0];
-	for(size_t i = denom.size() - 1; 0 < i; i--)
-	{
-		denvalue += denom[i];
-		denvalue *= value;
-	}
-	denvalue += denom[0];
-	return double(numvalue)/double(denvalue);
+	return double(numvalue);
 }
