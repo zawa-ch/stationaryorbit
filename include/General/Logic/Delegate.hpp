4077bbb2c96ee4ddff6bbf70ba79998d4f70b8ad
zawa-ch <lunatic.zawa.ch@gmail.com>
2019-11-15 22:18:12 +0900

    added Delegate

---
+++ include/General/Logic/Delegate.hpp
@@ -0,0 +1,31 @@
+#ifndef __StationaryOrbit_Delegate__
+#define __StationaryOrbit_Delegate__
+#include <set>
+namespace StationaryOrbit
+{
+
+	template<class argsT = void>
+	class Delegate
+	{
+	public:
+
+		typedef void (*Action)(argsT);
+
+	private:
+
+		std::set<Action> actions;
+
+	public:
+
+		void Add(Action action) { actions.insert(action); }
+
+		void Remove(Action action) { actions.erase(action); }
+
+		void Clear() { actions.clear(); }
+
+		void Invoke(argsT args) const { for(Action item : actions) { item(args); } }
+
+	};
+
+}
+#endif // __StationaryOrbit_Delegate__
