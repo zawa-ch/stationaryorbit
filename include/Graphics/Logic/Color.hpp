1d303cd9bdf2033a02ef903b510ab3eb1706c899
zawa-ch <lunatic.zawa.ch@gmail.com>
2019-12-17 16:48:10 +0900

    modified Graphics::Color
    Normalizeの機能を各色空間の構造体のNormalizeへの参照により実装

--- include/Graphics/Logic/Color.hpp
+++ include/Graphics/Logic/Color.hpp
@@ -15,11 +15,13 @@ namespace StationaryOrbit::Graphics
 		float elements[3];
 		float alpha;
 
+		///	内部の情報から @a RGBColor を構築します。
+		///	このメソッドを実行するには @a colorsys が @a ColorSystem::RGB である必要があります。
+		RGBColor ctorRGBColor() const;
+
 	public:
 
 		Color() = default;
-
-		explicit Color(const RGBColor& value);
 
 		Color(const RGBColor& value, float alpha = 1.0f);
 
@@ -29,8 +31,22 @@ namespace StationaryOrbit::Graphics
 		///	このオブジェクトの表色系を取得します。
 		ColorSystem getColorSystem() const;
 
-		///	このオブジェクトのRGB値を示すRGBColorオブジェクトを取得します。
+		///	このオブジェクトのRGB値を示す @a RGBColor オブジェクトを取得します。
 		RGBColor getRGB() const;
+
+		///	このオブジェクトが正規化されているかを取得します。
+		///
+		///	@return
+		///	正規化されている場合 @a true が返ります。
+		///	正規化されていない場合 @a false が返ります。
+		bool IsNormalized() const;
+
+		///	このオブジェクトを正規化した @a Color を取得します。
+		///
+		///	@return
+		///	正規化された @a Color オブジェクトが返ります。
+		///	内部の色空間は正規化される前の @a Color オブジェクトの色空間に依存します。
+		Color Normalize() const;
 
 	};
 
