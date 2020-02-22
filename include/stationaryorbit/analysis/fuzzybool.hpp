5dba8f7479b8f04ae0e8cb58e44ff20ed1c5b3ed
zawa-ch <lunatic.zawa.ch@gmail.com>
2020-02-22 17:54:15 +0900

    added NumericAnalysis::FuzzyBool

--- include/stationaryorbit/analysis/fuzzybool.hpp
+++ include/stationaryorbit/analysis/fuzzybool.hpp
@@ -8,27 +8,41 @@ namespace zawa_ch::StationaryOrbit::NumericAnalysis
 	struct FuzzyBool
 	{
 	private:
+
 		FractionalDec _value;
+
 	public:
+
 		FuzzyBool() = default;
-		FuzzyBool(bool value) : _value((value?1U:0U), 1U) {}
-		explicit FuzzyBool(const uintmax_t& num, const uintmax_t& den) : _value(num, den) {}
-		explicit FuzzyBool(const double& value) : _value(value) {}
-		explicit FuzzyBool(const FractionalDec& value) : _value(value) {}
+		FuzzyBool(bool value);
+		explicit FuzzyBool(const uintmax_t& num, const uintmax_t& den);
+		explicit FuzzyBool(const double& value);
+		explicit FuzzyBool(const FractionalDec& value);
+
 		///	指定された値でこのオブジェクトの二値化を行います。
-		bool Binalization(const FractionalDec& value) const { return value <= _value; }
-		FuzzyBool Not() const { return FuzzyBool(FractionalDec::Max() - _value); }
+		bool Binalization(const FractionalDec& value) const;
+
+		FuzzyBool Not() const;
 		FuzzyBool operator!() const { return Not(); }
-		FuzzyBool Or(const FuzzyBool& value) const { return FuzzyBool((value._value<_value)?_value:value._value); }
+
+		FuzzyBool Or(const FuzzyBool& value) const;
 		FuzzyBool operator||(const FuzzyBool& value) const { return Or(value); }
-		FuzzyBool And(const FuzzyBool& value) const { return FuzzyBool((_value<value._value)?_value:value._value); }
+
+		FuzzyBool And(const FuzzyBool& value) const;
 		FuzzyBool operator&&(const FuzzyBool& value) const { return And(value); }
-		FuzzyBool AlgebraAdd(const FuzzyBool& value) const { return FuzzyBool(_value + (value._value - _value * value._value)); }
-		FuzzyBool AlgebraProduct(const FuzzyBool& value) const { return FuzzyBool(_value * value._value); }
-		FuzzyBool LimitAdd(const FuzzyBool& value) const { return FuzzyBool(_value + value._value); }
-		FuzzyBool LimitProduct(const FuzzyBool& value) const { return FuzzyBool(_value - (FractionalDec::Max() - value._value)); }
-		static FuzzyBool True() { return FuzzyBool(true); }
-		static FuzzyBool False() { return FuzzyBool(false); }
+
+		FuzzyBool AlgebraAdd(const FuzzyBool& value) const;
+
+		FuzzyBool AlgebraProduct(const FuzzyBool& value) const;
+
+		FuzzyBool LimitAdd(const FuzzyBool& value) const;
+
+		FuzzyBool LimitProduct(const FuzzyBool& value) const;
+
+		static FuzzyBool True();
+
+		static FuzzyBool False();
+
 	};
 
 }
---
+++ src/analysis/fuzzybool.cpp
@@ -0,0 +1,48 @@
+#include "stationaryorbit/analysis/fuzzybool.hpp"
+using namespace zawa_ch::StationaryOrbit::NumericAnalysis;
+
+FuzzyBool::FuzzyBool(bool value)
+    : _value((value?1U:0U), 1U)
+{}
+
+FuzzyBool::FuzzyBool(const uintmax_t& num, const uintmax_t& den)
+    : _value(num, den)
+{}
+
+FuzzyBool::FuzzyBool(const double& value)
+    : _value(value)
+{}
+
+FuzzyBool::FuzzyBool(const FractionalDec& value)
+    : _value(value)
+{}
+
+bool FuzzyBool::Binalization(const FractionalDec& value) const
+{ return value <= _value; }
+
+FuzzyBool FuzzyBool::Not() const
+{ return FuzzyBool(FractionalDec::Max() - _value); }
+
+FuzzyBool FuzzyBool::Or(const FuzzyBool& value) const
+{ return FuzzyBool((value._value<_value)?_value:value._value); }
+
+FuzzyBool FuzzyBool::And(const FuzzyBool& value) const
+{ return FuzzyBool((_value<value._value)?_value:value._value); }
+
+FuzzyBool FuzzyBool::AlgebraAdd(const FuzzyBool& value) const
+{ return FuzzyBool(_value + (value._value - _value * value._value)); }
+
+FuzzyBool FuzzyBool::AlgebraProduct(const FuzzyBool& value) const
+{ return FuzzyBool(_value * value._value); }
+
+FuzzyBool FuzzyBool::LimitAdd(const FuzzyBool& value) const
+{ return FuzzyBool(_value + value._value); }
+
+FuzzyBool FuzzyBool::LimitProduct(const FuzzyBool& value) const
+{ return FuzzyBool(_value - (FractionalDec::Max() - value._value)); }
+
+FuzzyBool FuzzyBool::True()
+{ return FuzzyBool(true); }
+
+FuzzyBool FuzzyBool::False()
+{ return FuzzyBool(false); }
