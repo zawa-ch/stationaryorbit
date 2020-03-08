281fcf5eddf04c72eade02af7ae00e4b7d3ec24d
zawa-ch <lunatic.zawa.ch@gmail.com>
2020-03-09 00:29:22 +0900

    added basetypes.hpp
    インターフェースと基本クラスを集約
    pixelreferenceを吸収

---
+++ include/stationaryorbit/graphics-core/basetypes.hpp
@@ -0,0 +1,84 @@
+#ifndef __stationaryorbit_graphics_core_basetypes__
+#define __stationaryorbit_graphics_core_basetypes__
+#include <cstddef>
+#include "channelvalue.hpp"
+#include "rgbcolor.hpp"
+namespace zawa_ch::StationaryOrbit::Graphics
+{
+
+	///	画像の大きさを取得するためのメソッドを実装します。
+    class IImageSize
+    {
+    public:
+		///	このバッファの幅を取得します。
+		virtual size_t GetHorizonalSize() const = 0;
+		///	このバッファの高さを取得します。
+		virtual size_t GetVerticalSize() const = 0;
+    };
+
+	///	ビットマップ画像を表します。
+	///	このクラスは抽象クラスです。
+	class Bitmap
+        : virtual public IImageSize
+	{
+	public:
+		///	指定されたピクセルの値をRGBで取得します。
+		virtual RGBColor GetRGB(const size_t& x, const size_t& y) const = 0;
+		///	指定されたピクセルの値をRGBで設定します。
+		virtual void SetRGB(const size_t& x, const size_t& y, const RGBColor& value) = 0;
+	};
+
+	///	ビットマップ画像のデータ構造として用いる色空間を表します。
+	enum class BitmapColorSpace
+	{
+		///	グレースケール。
+		GrayScale,
+		///	RGB(+アルファ)色空間。
+		ARGB,
+		///	CMYK色空間。
+		CMYK,
+		///	XYZ(+アルファ)色空間。
+		AXYZ
+	};
+
+	///	画像情報を保持するための記憶領域を提供し、アクセスを行うためのメソッドを実装します。
+	///	@param	T
+	///	値の表現に使用する型。
+	template<class T>
+    class IBitmapBuffer
+		: virtual public IImageSize
+    {
+    public:
+		///	バッファに使用されている色空間を取得します。
+		virtual BitmapColorSpace GetColorSpace() const = 0;
+		///	指定された1ピクセル・1チャネルにおける値を取得します。
+		virtual const ChannelValue<T>& Index(const size_t& x, const size_t& y, const size_t& ch) const = 0;
+		///	指定された1ピクセル・1チャネルにおける値を取得します。
+		virtual ChannelValue<T>& Index(const size_t& x, const size_t& y, const size_t& ch) = 0;
+    };
+
+	/// 画像上のピクセルへの参照を表します。
+	/// このクラスは抽象クラスです。
+	class PixelReference
+	{
+	public:
+		///	参照先の値を @a RGBColor で取得します。
+		virtual RGBColor GetRGBValue() const = 0;
+		///	参照先に @a RGBColor の値を設定します。
+		virtual void SetValue(const RGBColor& value) = 0;
+		///	参照先の値に @a BitmapPixelGetter の参照先の値を設定します。
+		virtual void SetValue(const PixelReference& value) = 0;
+		///	このオブジェクトの参照先が存在するかを取得します。
+		virtual bool HasValue() const noexcept = 0;
+		///	この参照にオフセットを加えた先に、参照先が存在するかを取得します。
+		virtual bool HasOffset(const Point& offset) const noexcept = 0;
+		///	この参照にオフセットを加えた先の値を @a RGBColor で取得します。
+		virtual RGBColor GetOffsetRGBValue(const Point& offset) const = 0;
+		///	この参照にオフセットを加えた先に @a RGBColor の値を設定します。
+		virtual void SetOffsetRGBValue(const Point& offset, const RGBColor& value) = 0;
+		///	この参照にオフセットを加えた先の値に @a BitmapPixelGetter の参照先の値を設定します。
+		virtual void SetOffsetRGBValue(const Point& offset, const PixelReference& value) = 0;
+	};
+
+}
+#endif // __stationaryorbit_graphics_core_basetypes__
