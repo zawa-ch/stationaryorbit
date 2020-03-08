ec0cfc234adc11237f12df1a18c99fa6103e53da
zawa-ch <lunatic.zawa.ch@gmail.com>
2020-03-08 20:42:11 +0900

    Graphics::BitmapPixelReference
    インターフェイスの抽出

--- include/stationaryorbit/graphics-core/bitmappixelreference.hpp
+++ include/stationaryorbit/graphics-core/bitmappixelreference.hpp
@@ -1,20 +1,19 @@
-#ifndef __StationaryOrbit_Graphics_BitmapPixelReference__
-#define __StationaryOrbit_Graphics_BitmapPixelReference__
+#ifndef __stationaryorbit_graphics_core_bitmappixelreference__
+#define __stationaryorbit_graphics_core_bitmappixelreference__
 #include "stationaryorbit/exception/soexcept"
 #include "point.hpp"
 #include "rgbcolor.hpp"
+#include "pixelreference.hpp"
 #include "bitmapbuffer.hpp"
 #include "imageinfomation.hpp"
 #include "graphicscore.hpp"
 namespace zawa_ch::StationaryOrbit::Graphics
 {
 
-	///	Bitmapのピクセルへの参照を表します。
-	///	@note
-	///	このオブジェクトは指定されたオブジェクトへの参照を保持します。
-	///	このとき、参照先にオブジェクトが存在するかどうかは確認されません。
+	///	@a BitmapBuffer 上のピクセルへの参照を表します。
 	template<class T>
 	class BitmapPixelReference final
+		: virtual public PixelReference
 	{
 	public: // type
 
@@ -38,10 +37,7 @@ namespace zawa_ch::StationaryOrbit::Graphics
 		///	参照先のx座標。
 		///	@param	y
 		///	参照先のy座標。
-		BitmapPixelReference(IBufferType& buffer, const size_t& x, const size_t& y) : _buf(buffer), _x(x), _y(y)
-		{
-			if (!HasValue(buffer, x, y)) { throw std::out_of_range("指定された参照はビットマップ領域の範囲外です。"); }
-		}
+		BitmapPixelReference(IBufferType& buffer, const size_t& x, const size_t& y) : _buf(buffer), _x(x), _y(y) {}
 
 		///	@a Bitmap のピクセルへの参照を作成します。
 		///	@param	buffer
@@ -50,7 +46,7 @@ namespace zawa_ch::StationaryOrbit::Graphics
 		///	参照先の座標。
 		BitmapPixelReference(IBufferType& buffer, const Point& position) : BitmapPixelReference(buffer, position.getX(), position.getY())
 		{
-			if ((position.getX() < 0)||(position.getY() < 0)) { throw std::invalid_argument("負の値を持つpositionを引数に取りました。"); }
+			if ((position.getX() < 0)||(position.getY() < 0)) { throw std::invalid_argument("負の値を持つ position を引数に取りました。"); }
 		}
 
 	public: // member
@@ -62,91 +58,98 @@ namespace zawa_ch::StationaryOrbit::Graphics
 		IBufferType& Buffer() noexcept { return _buf; }
 
 		///	この参照が指し示す座標を @a Point で取得します。
-		Point getPosition() const noexcept { return Point(_x, _y); }
+		Point Position() const noexcept { return Point(_x, _y); }
+
+		///	この参照に指定されたオフセットを加えた参照を取得します。
+		///	@param	[in]offset
+		///	座標のオフセット。
+		///	@return
+		///	オフセットを加えた参照が返ります。
+		BitmapPixelReference Offset(const Point& offset) { return BitmapPixelReference<T>(_buf, Position() + offset); }
+
+		///	この参照に指定されたオフセットを加えた参照を取得します。
+		///	@param	[in]offset
+		///	座標のオフセット。
+		///	@return
+		///	オフセットを加えた参照が返ります。
+		const BitmapPixelReference Offset(const Point& offset) const { return BitmapPixelReference<T>(_buf, Position() + offset); }
+
+	public: // implement PixelReference
 
 		///	参照先の値を @a RGBColor で取得します。
-		RGBColor getRGBValue() const
+		RGBColor GetRGBValue() const
 		{
+			if (!HasValue()) { throw std::out_of_range("参照先が無効です。"); }
 			switch (_buf.getColorSpace())
 			{
-				case BufferColorSpace::ARGB: return RGBColor(refChannel(0).ConvertTo<float>().value, refChannel(1).ConvertTo<float>().value, refChannel(2).ConvertTo<float>().value, refChannel(3).ConvertTo<float>().value);
-				case BufferColorSpace::GrayScale:
-				case BufferColorSpace::CMYK:
-				case BufferColorSpace::AXYZ:
-				default: throw InvalidOperationException("バッファに指定されている色空間が無効です。");
+			case BufferColorSpace::ARGB:
+				return RGBColor(
+					ChannelValue<float>(refChannel(0)).value,
+					ChannelValue<float>(refChannel(1)).value,
+					ChannelValue<float>(refChannel(2)).value,
+					ChannelValue<float>(refChannel(3)).value);
+			case BufferColorSpace::GrayScale:
+			case BufferColorSpace::CMYK:
+			case BufferColorSpace::AXYZ:
+			default: throw InvalidOperationException("バッファに指定されている色空間が無効です。");
+			}
+		}
+
+		///	参照先に @a RGBColor の値を設定します。
+		void SetValue(const RGBColor& value)
+		{
+			if (!HasValue()) { throw std::out_of_range("参照先が無効です。"); }
+			switch (_buf.getColorSpace())
+			{
+			case BufferColorSpace::ARGB:
+				refChannel(0) = ChannelValue<T>(value.getR());
+				refChannel(1) = ChannelValue<T>(value.getG());
+				refChannel(2) = ChannelValue<T>(value.getB());
+				refChannel(3) = ChannelValue<T>(value.getAlpha());
+				break;
+			case BufferColorSpace::GrayScale:
+			case BufferColorSpace::CMYK:
+			case BufferColorSpace::AXYZ:
+			default: throw InvalidOperationException("バッファに指定されている色空間が無効です。");
 			}
 		}
 
 		///	参照先の値に @a BitmapPixelGetter の参照先の値を設定します。
-		void setValue(const RGBColor& value)
+		void SetValue(const PixelReference& value)
 		{
-			switch (_buf.getColorSpace())
+			if (!HasValue()) { throw std::out_of_range("参照先が無効です。"); }
+			switch(_buf.getColorSpace())
 			{
-				case BufferColorSpace::ARGB:
-					refChannel(0) = ChannelValue<T>::ConvertFrom(value.getR());
-					refChannel(1) = ChannelValue<T>::ConvertFrom(value.getG());
-					refChannel(2) = ChannelValue<T>::ConvertFrom(value.getB());
-					refChannel(3) = ChannelValue<T>::ConvertFrom(value.getAlpha());
-					break;
-				case BufferColorSpace::GrayScale:
-				case BufferColorSpace::CMYK:
-				case BufferColorSpace::AXYZ:
-				default: throw InvalidOperationException("バッファに指定されている色空間が無効です。");
+			case BufferColorSpace::ARGB:
+				SetValue(value.GetRGBValue());
+				break;
+			case BufferColorSpace::GrayScale:
+			case BufferColorSpace::CMYK:
+			case BufferColorSpace::AXYZ:
+			default: throw InvalidOperationException("バッファに指定されている色空間が無効です。");
 			}
 		}
 
-		///	この参照に指定されたオフセットを加えた参照に、参照先が存在するかを取得します。
-		///
-		///	@param	[in]offset
-		///	座標のオフセット。
-		///
-		///	@return
-		///	参照先が存在すれば @a true 、存在しなければ @a false が返ります。
-		bool HasOffset(const Point& offset) const noexcept { return HasValue(_buf, getPosition() + offset); }
-
-		///	この参照に指定されたオフセットを加えた参照を取得します。
-		///	@param	[in]offset
-		///	座標のオフセット。
-		///	@return
-		///	オフセットを加えた参照が返ります。
-		BitmapPixelReference Offset(const Point& offset) { return BitmapPixelReference<T>(_buf, getPosition() + offset); }
-
-		///	この参照に指定されたオフセットを加えた参照を取得します。
-		///	@param	[in]offset
-		///	座標のオフセット。
-		///	@return
-		///	オフセットを加えた参照が返ります。
-		const BitmapPixelReference Offset(const Point& offset) const { return BitmapPixelReference<T>(_buf, getPosition() + offset); }
-
-	private: // internal
-
-		///	指定したBitmapBufferの指定した座標が存在するかを取得します。
-		///	@param	buffer
-		///	参照先の @a BitmapBuffer 。
-		///	@param	x
-		///	参照先のx座標。
-		///	@param	y
-		///	参照先のy座標。
-		///	@return
-		///	参照先にピクセルが存在すれば @a true 、そうでなければ @a false が返ります。
-		static bool HasValue(const IBufferType& buffer, const size_t& x, const size_t& y) noexcept
+		///	このオブジェクトの参照先が存在するかを取得します。
+		bool HasValue() const noexcept
 		{
-			if ((buffer.getHorizonalSize() > x)&&(buffer.getVerticalSize() > y)) { return true; }
+			if ((_buf.getHorizonalSize() > _x)&&(_buf.getVerticalSize() > _y)) { return true; }
 			else { return false; }
 		}
 
-		///	指定したBitmapBufferの指定した座標が存在するかを取得します。
-		///	@param	buffer
-		///	参照先の @a BitmapBuffer 。
-		///	@param	pos
-		///	参照先の座標。
-		///	@return
-		///	参照先にピクセルが存在すれば @a true 、そうでなければ @a false が返ります。
-		static bool HasValue(const IBufferType& buffer, const Point& pos) noexcept
-		{
-			if ((pos.getX() < 0)||(pos.getY() < 0)) { return false; }
-			return HasValue(buffer, pos.getX(), pos.getY());
-		}
+		///	この参照にオフセットを加えた先に、参照先が存在するかを取得します。
+		bool HasOffset(const Point& offset) const noexcept { return Offset(offset).HasValue(); }
+
+		///	この参照にオフセットを加えた先の値を @a RGBColor で取得します。
+		RGBColor GetOffsetRGBValue(const Point& offset) const { return Offset(offset).GetRGBValue(); }
+
+		///	この参照にオフセットを加えた先に @a RGBColor の値を設定します。
+		void SetOffsetRGBValue(const Point& offset, const RGBColor& value) { return Offset(offset).SetValue(value); }
+
+		///	この参照にオフセットを加えた先の値に @a BitmapPixelGetter の参照先の値を設定します。
+		void SetOffsetRGBValue(const Point& offset, const PixelReference& value) { return Offset(offset).SetValue(value); }
+
+	private: // internal
 
 		const ChannelValue<T>& refChannel(const size_t& channel) const { return _buf.Index(_x, _y, channel); }
 
@@ -155,4 +158,4 @@ namespace zawa_ch::StationaryOrbit::Graphics
 	};
 
 }
-#endif // __StationaryOrbit_Graphics_BitmapPixelReference__
+#endif // __stationaryorbit_graphics_core_bitmappixelreference__
