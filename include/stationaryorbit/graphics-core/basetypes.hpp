0fd47de09ad4ba9b935930627e031ed03ed518b0
zawa-ch <lunatic.zawa.ch@gmail.com>
2020-03-10 00:48:59 +0900

    Graphics::PixelReference
    宣言位置変更

--- include/stationaryorbit/graphics-core/basetypes.hpp
+++ include/stationaryorbit/graphics-core/basetypes.hpp
@@ -45,29 +45,5 @@ namespace zawa_ch::StationaryOrbit::Graphics
 		virtual ~Bitmap() = default;
 	};
 
-	/// 画像上のピクセルへの参照を表します。
-	/// このクラスは抽象クラスです。
-	class PixelReference
-	{
-	public:
-		///	参照先の値を @a RGBColor で取得します。
-		virtual RGBColor GetRGBValue() const = 0;
-		///	参照先に @a RGBColor の値を設定します。
-		virtual void SetValue(const RGBColor& value) = 0;
-		///	参照先の値に @a BitmapPixelGetter の参照先の値を設定します。
-		virtual void SetValue(const PixelReference& value) = 0;
-		///	このオブジェクトの参照先が存在するかを取得します。
-		virtual bool HasValue() const noexcept = 0;
-		///	この参照にオフセットを加えた先に、参照先が存在するかを取得します。
-		virtual bool HasOffset(const Point& offset) const noexcept = 0;
-		///	この参照にオフセットを加えた先の値を @a RGBColor で取得します。
-		virtual RGBColor GetOffsetRGBValue(const Point& offset) const = 0;
-		///	この参照にオフセットを加えた先に @a RGBColor の値を設定します。
-		virtual void SetOffsetRGBValue(const Point& offset, const RGBColor& value) = 0;
-		///	この参照にオフセットを加えた先の値に @a BitmapPixelGetter の参照先の値を設定します。
-		virtual void SetOffsetRGBValue(const Point& offset, const PixelReference& value) = 0;
-		virtual ~PixelReference() = default;
-	};
-
 }
 #endif // __stationaryorbit_graphics_core_basetypes__
--- include/stationaryorbit/graphics-core/bitmappixelreference.hpp
+++ include/stationaryorbit/graphics-core/bitmappixelreference.hpp
@@ -3,9 +3,34 @@
 #include "stationaryorbit/exception/soexcept"
 #include "point.hpp"
 #include "channelvalue.hpp"
-#include "basetypes.hpp"
+#include "rgbcolor.hpp"
+#include "bitmapbuffer.hpp"
 namespace zawa_ch::StationaryOrbit::Graphics
 {
+
+	/// 画像上のピクセルへの参照を表します。
+	/// このクラスは抽象クラスです。
+	class PixelReference
+	{
+	public:
+		///	参照先の値を @a RGBColor で取得します。
+		virtual RGBColor GetRGBValue() const = 0;
+		///	参照先に @a RGBColor の値を設定します。
+		virtual void SetValue(const RGBColor& value) = 0;
+		///	参照先の値に @a BitmapPixelGetter の参照先の値を設定します。
+		virtual void SetValue(const PixelReference& value) = 0;
+		///	このオブジェクトの参照先が存在するかを取得します。
+		virtual bool HasValue() const noexcept = 0;
+		///	この参照にオフセットを加えた先に、参照先が存在するかを取得します。
+		virtual bool HasOffset(const Point& offset) const noexcept = 0;
+		///	この参照にオフセットを加えた先の値を @a RGBColor で取得します。
+		virtual RGBColor GetOffsetRGBValue(const Point& offset) const = 0;
+		///	この参照にオフセットを加えた先に @a RGBColor の値を設定します。
+		virtual void SetOffsetRGBValue(const Point& offset, const RGBColor& value) = 0;
+		///	この参照にオフセットを加えた先の値に @a BitmapPixelGetter の参照先の値を設定します。
+		virtual void SetOffsetRGBValue(const Point& offset, const PixelReference& value) = 0;
+		virtual ~PixelReference() = default;
+	};
 
 	///	@a BitmapBuffer 上のピクセルへの参照を表します。
 	template<class T>
