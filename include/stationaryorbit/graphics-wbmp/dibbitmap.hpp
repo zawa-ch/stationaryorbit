4af808db0af6d50b45c4a7af0e1e8d9b97a3ef58
zawa-ch <lunatic.zawa.ch@gmail.com>
2020-03-26 00:50:58 +0900

    + Graphics::DIB::DibBitmap

---
+++ include/stationaryorbit/graphics-wbmp/dibbitmap.hpp
@@ -0,0 +1,8 @@
+#ifndef __stationaryorbit_graphics_dib_dibbitmap__
+#define __stationaryorbit_graphics_dib_dibbitmap__
+#include "stationaryorbit/graphics-core/bitmap"
+namespace zawa_ch::StationaryOrbit::Graphics::DIB
+{
+	typedef ARGBBitmapImageBase<uint8_t> DibBitmap;
+}
+#endif // __stationaryorbit_graphics_dib_dibbitmap__
