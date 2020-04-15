10439d8e89c37c4dc8b51ea6383bb0fe53ba29e2
zawa-ch <lunatic.zawa.ch@gmail.com>
2020-04-15 21:42:30 +0900

    Graphics::ChannelValue::ConvertTo()

--- include/stationaryorbit/graphics-core/channelvalue.hpp
+++ include/stationaryorbit/graphics-core/channelvalue.hpp
@@ -24,7 +24,7 @@ namespace zawa_ch::StationaryOrbit::Graphics
 		constexpr explicit ChannelValue(const ValueType& value) : value(value) {}
 		constexpr explicit ChannelValue(ValueType&& value) : value(std::exchange(value, ValueType())) {}
 		template<class fromT>
-		constexpr explicit ChannelValue(const ChannelValue<fromT>& from) : ChannelValue(ConvertFrom(from)) {}
+		constexpr explicit ChannelValue(const ChannelValue<fromT>& from) : ChannelValue(from.template ConvertTo<Tp>()) {}
 	public: // member
 		constexpr bool IsNormalized() const { return (Min().value < value)&&(value < Max().value); }
 	public: // static
@@ -81,28 +81,32 @@ namespace zawa_ch::StationaryOrbit::Graphics
 		ChannelValue<Tp>& operator=(ValueType&& value) { std::swap(this->value, value); return *this; }
 	public: // convert
 		constexpr operator ValueType() const { return value; }
-		template<class fromT>
-		constexpr static ChannelValue<Tp> ConvertFrom(const ChannelValue<fromT>& from)
+		template<class castT>
+		constexpr ChannelValue<castT> ConvertTo() const
 		{
-			static_assert((std::is_floating_point_v<fromT>)||(std::is_integral_v<fromT>), "テンプレートの特殊化に失敗しました(テンプレート型 fromT は整数型または浮動小数点数型のいずれでもありません)。");
 			static_assert((std::is_floating_point_v<Tp>)||(std::is_integral_v<Tp>), "テンプレートの特殊化に失敗しました(テンプレート型 Tp は整数型または浮動小数点数型のいずれでもありません)。");
+			static_assert((std::is_floating_point_v<castT>)||(std::is_integral_v<castT>), "テンプレートの特殊化に失敗しました(テンプレート型 castT は整数型または浮動小数点数型のいずれでもありません)。");
 			if constexpr (std::is_floating_point_v<Tp>)
 			{
-				if constexpr (std::is_floating_point_v<fromT>) { return ChannelValue(Tp(from.value)); }
-				if constexpr (std::is_integral_v<fromT>) { return ChannelValue(Tp(from.value) / ChannelValue<Tp>::Max().value); }
+				// float -> float
+				if constexpr (std::is_floating_point_v<castT>) { return ChannelValue<castT>(castT(value)); }
+				// float -> integer
+				if constexpr (std::is_integral_v<castT>) { return ChannelValue<castT>(castT(value * ChannelValue<castT>::Max().value)); }
 			}
 			if constexpr (std::is_integral_v<Tp>)
 			{
-				if constexpr (std::is_floating_point_v<fromT>) { return ChannelValue(Tp(from.value * ChannelValue<Tp>::Max().value)); }
-				if constexpr (std::is_integral_v<fromT>)
+				// integer -> float
+				if constexpr (std::is_floating_point_v<castT>) { return ChannelValue<castT>(castT(value) / Max().value); }
+				// integer -> integer
+				if constexpr (std::is_integral_v<castT>)
 				{
-					if constexpr (sizeof(fromT) < sizeof(Tp)) { return ChannelValue(Tp(from.value) * (ChannelValue<Tp>::Max().value / ChannelValue<fromT>::Max().value)); }
-					else { return ChannelValue(Tp(from.value / (ChannelValue<fromT>::Max().value / ChannelValue<Tp>::Max().value))); }
+					if constexpr (sizeof(Tp) < sizeof(castT)) { return ChannelValue<castT>(castT(value) * (ChannelValue<castT>::Max().value / Max().value)); }
+					else { return ChannelValue<castT>(castT(value / (Max().value / ChannelValue<castT>::Max().value))); }
 				}
 			}
 		}
 	public: // copy/move/destruct
-		constexpr ChannelValue(const ChannelValue<Tp>&) = default;
+		constexpr ChannelValue<Tp>(const ChannelValue<Tp>&) = default;
 		ChannelValue<Tp>& operator=(const ChannelValue<Tp>& value) = default;
 		constexpr ChannelValue(ChannelValue<Tp>&&) = default;
 		ChannelValue<Tp>& operator=(ChannelValue<Tp>&& value) = default;
