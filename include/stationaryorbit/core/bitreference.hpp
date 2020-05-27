b55a72217f6c29d101aa7353bd6b06a1b960ed69
zawa-ch <lunatic.zawa.ch@gmail.com>
2020-05-27 12:24:21 +0900

    BitReference: ソースコードフォーマッティング

--- include/stationaryorbit/core/bitreference.hpp
+++ include/stationaryorbit/core/bitreference.hpp
@@ -1,35 +1,23 @@
-#ifndef __StationaryOrbit_BitReference__
-#define __StationaryOrbit_BitReference__
+#ifndef __stationaryorbit_core_bitreference__
+#define __stationaryorbit_core_bitreference__
 #include <cstddef>
 #include <stdexcept>
 namespace zawa_ch::StationaryOrbit
 {
-
 	///	特定のビットへの参照を保持し、ビット単位でのアクセスを行います。
 	class BitReference
 	{
 	private:
-
 		std::byte& _ref;	///< 参照先の1バイトビット列。
 		size_t _pos;	///< 参照先のビット位置。
-
 	public:
-
 		///	特定のビットへの参照を作成します。
-		constexpr BitReference(std::byte& reference, const size_t& bitpos)
-			: _ref(reference), _pos(bitpos)
-		{
-			if (8 <= bitpos) throw std::out_of_range("The argment 'bitpos' must be between 0 and 7.");
-		}
-
+		constexpr BitReference(std::byte& reference, const size_t& bitpos) : _ref(reference), _pos(bitpos) { if (8 <= bitpos) throw std::out_of_range("bitpos は0から7の範囲内である必要があります。"); }
 		///	このオブジェクトが参照しているstd::byteの参照を取得します。
 		constexpr std::byte& getByte() { return _ref; }
-
 		///	ビット参照の逆参照を行い、boolに変換します。
-		constexpr bool getValue() const
-		{ return (_ref & std::byte(0x01 << _pos)) != std::byte(0); }
+		constexpr bool getValue() const { return (_ref & std::byte(0x01 << _pos)) != std::byte(0); }
 		constexpr operator bool() const { return getValue(); }
-
 		///	参照先に値を代入します。
 		constexpr BitReference& Assign(const bool& value)
 		{
@@ -37,8 +25,7 @@ namespace zawa_ch::StationaryOrbit
 			return *this;
 		}
 		constexpr BitReference& operator=(const bool& value) { return Assign(value); }
-
 	};
 
 }
-#endif // __StationaryOrbit_BitReference__
+#endif // __stationaryorbit_core_bitreference__
