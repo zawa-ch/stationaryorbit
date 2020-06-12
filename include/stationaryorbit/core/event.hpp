a9ffbced010d5192aa5aa5022a0f5e84af3766ad
zawa-ch <lunatic.zawa.ch@gmail.com>
2020-06-12 23:25:46 +0900

    イベント引数型

---
+++ include/stationaryorbit/core/event.hpp
@@ -0,0 +1,8 @@
+#ifndef __stationaryorbit_core_event__
+#define __stationaryorbit_core_event__
+namespace zawea_ch::StationaryOrbit
+{
+    /// イベントコール時に付加される追加の情報を表す基本クラスです。
+    class EventArgs {};
+}
+#endif // __stationaryorbit_core_event__
