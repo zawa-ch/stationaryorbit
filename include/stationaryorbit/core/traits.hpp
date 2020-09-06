25e82dffd61a72f05d9ff497eaa7ee6a14bb1d9e
zawa-ch <satellite.2e1834097@gmail.com>
2020-09-06 12:38:48 +0900

    [WIP] Traitsの追加

--- include/stationaryorbit/core/traits.hpp
+++ include/stationaryorbit/core/traits.hpp
@@ -1,3 +1,21 @@
+//	stationaryorbit/core/traits
+//	Copyright 2020 zawa-ch.
+//	LGPLv3 (or later) license
+//
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU Lesser General Public License as
+//	published by the Free Software Foundation, either version 3 of the
+//	License, or any later version.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//	See the GNU Lesser General Public License for more details.
+//
+//	You should have received a copy of the GNU Lesser General Public
+//	License along with this program.
+//	If not, see <http://www.gnu.org/licenses/>.
+//
 #ifndef __stationaryorbit_core_traits__
 #define __stationaryorbit_core_traits__
 #include <type_traits>
@@ -15,28 +33,144 @@ namespace zawa_ch::StationaryOrbit
 		Traits(Traits&&) = delete;
 		~Traits() = delete;
 
-		template<class, class, class = std::void_t<>>
-		struct Addable_t : std::false_type {};
-		template<class T, class U>
-		struct Addable_t<T, U, std::void_t< decltype( std::declval<T&>() + std::declval<U&>() ) > > : std::true_type {};
+		template<class, class = std::void_t<>>
+		struct HasPromotion_t : std::false_type {};
+		template<class T>
+		struct HasPromotion_t<T, std::void_t< decltype( +std::declval<T&>() ) > >
+			: std::is_convertible< decltype( +std::declval<T&>() ), T> {};
+
+		template<class, class = std::void_t<>>
+		struct HasInverse_t : std::false_type {};
+		template<class T>
+		struct HasInverse_t<T, std::void_t< decltype( -std::declval<T&>() ) > >
+			: std::is_convertible< decltype( -std::declval<T&>() ), T> {};
 
 		template<class, class, class = std::void_t<>>
-		struct Subtractable_t : std::false_type {};
+		struct HasAddition_t : std::false_type {};
 		template<class T, class U>
-		struct Subtractable_t<T, U, std::void_t< decltype( std::declval<T&>() - std::declval<U&>() ) > > : std::true_type {};
+		struct HasAddition_t<T, U, std::void_t< decltype( std::declval<T&>() + std::declval<U&>() ) > >
+			: std::is_convertible< decltype( std::declval<T&>() + std::declval<U&>() ), T> {};
+
+		template<class, class, class = std::void_t<>>
+		struct HasSubtraction_t : std::false_type {};
+		template<class T, class U>
+		struct HasSubtraction_t<T, U, std::void_t< decltype( std::declval<T&>() - std::declval<U&>() ) > >
+			: std::is_convertible< decltype( std::declval<T&>() - std::declval<U&>() ), T> {};
 
 		template<class, class, class = std::void_t<>>
 		struct HasMultiplication_t : std::false_type {};
 		template<class T, class U>
-		struct HasMultiplication_t<T, U, std::void_t< decltype( std::declval<T&>() * std::declval<U&>() ) > > : std::true_type {};
+		struct HasMultiplication_t<T, U, std::void_t< decltype( std::declval<T&>() * std::declval<U&>() ) > >
+			: std::is_convertible< decltype( std::declval<T&>() * std::declval<U&>() ), T> {};
 
 		template<class, class, class = std::void_t<>>
 		struct HasDivision_t : std::false_type {};
 		template<class T, class U>
-		struct HasDivision_t<T, U, std::void_t< decltype( std::declval<T&>() / std::declval<U&>() ) > > : std::true_type {};
+		struct HasDivision_t<T, U, std::void_t< decltype( std::declval<T&>() / std::declval<U&>() ) > >
+			: std::is_convertible< decltype( std::declval<T&>() / std::declval<U&>() ), T> {};
+
+		template<class, class, class = std::void_t<>>
+		struct HasModulation_t : std::false_type {};
+		template<class T, class U>
+		struct HasModulation_t<T, U, std::void_t< decltype( std::declval<T&>() % std::declval<U&>() ) > >
+			: std::is_convertible< decltype( std::declval<T&>() % std::declval<U&>() ), T> {};
+
+		template<class, class = std::void_t<>>
+		struct HasArithmeticNot_t : std::false_type {};
+		template<class T>
+		struct HasArithmeticNot_t<T, std::void_t< decltype( ~std::declval<T&>() ) > >
+			: std::is_convertible< decltype( ~std::declval<T&>() ), T> {};
+
+		template<class, class, class = std::void_t<>>
+		struct HasArithmeticOr_t : std::false_type {};
+		template<class T, class U>
+		struct HasArithmeticOr_t<T, U, std::void_t< decltype( std::declval<T&>() | std::declval<U&>() ) > >
+			: std::is_convertible< decltype( std::declval<T&>() | std::declval<U&>() ), T> {};
+
+		template<class, class, class = std::void_t<>>
+		struct HasArithmeticAnd_t : std::false_type {};
+		template<class T, class U>
+		struct HasArithmeticAnd_t<T, U, std::void_t< decltype( std::declval<T&>() & std::declval<U&>() ) > >
+			: std::is_convertible< decltype( std::declval<T&>() & std::declval<U&>() ), T> {};
+
+		template<class, class, class = std::void_t<>>
+		struct HasArithmeticXor_t : std::false_type {};
+		template<class T, class U>
+		struct HasArithmeticXor_t<T, U, std::void_t< decltype( std::declval<T&>() ^ std::declval<U&>() ) > >
+			: std::is_convertible< decltype( std::declval<T&>() ^ std::declval<U&>() ), T> {};
+
+		template<class, class, class = std::void_t<>>
+		struct HasLShift_t : std::false_type {};
+		template<class T, class U>
+		struct HasLShift_t<T, U, std::void_t< decltype( std::declval<T&>() << std::declval<U&>() ) > >
+			: std::is_convertible< decltype( std::declval<T&>() << std::declval<U&>() ), T> {};
+
+		template<class, class, class = std::void_t<>>
+		struct HasRShift_t : std::false_type {};
+		template<class T, class U>
+		struct HasRShift_t<T, U, std::void_t< decltype( std::declval<T&>() >> std::declval<U&>() ) > >
+			: std::is_convertible< decltype( std::declval<T&>() >> std::declval<U&>() ), T> {};
+
+		template<class T>
+		struct HasUnaryOperation_t : std::conjunction<HasPromotion_t<T>, HasInverse_t<T>> {};
 
 		template<class T, class U>
-		struct ArithmeticOperatable_t : std::conjunction<Addable_t<T, U>, Subtractable_t<T, U>, HasMultiplication_t<T, U>, HasDivision_t<T, U>> {};
+		struct HasAdditiveOperation_t : std::conjunction<HasAddition_t<T, U>, HasSubtraction_t<T, U>> {};
+
+		template<class T, class U>
+		struct HasMultiplicativeOperation_t : std::conjunction<HasMultiplication_t<T, U>, HasDivision_t<T, U>> {};
+
+		template<class T, class U>
+		struct HasArithmeticOperation_t : std::conjunction<HasUnaryOperation_t<T>, HasAdditiveOperation_t<T, U>, HasMultiplicativeOperation_t<T, U>> {};
+
+		template<class T, class U>
+		struct HasBitOperation_t : std::conjunction<HasArithmeticNot_t<T>, HasArithmeticOr_t<T, U>, HasArithmeticAnd_t<T, U>, HasArithmeticXor_t<T, U>, HasLShift_t<T, U>, HasRShift_t<T, U>> {};
+
+		template<class, class, class = std::void_t<>>
+		struct HasSaturateAdd_t : std::false_type {};
+		template<class T, class U>
+		struct HasSaturateAdd_t<T, U, std::void_t< decltype( std::declval<T&>().SaturateAdd(std::declval<U&>()) ) > > : std::true_type {};
+
+		template<class, class, class = std::void_t<>>
+		struct HasSaturateSub_t : std::false_type {};
+		template<class T, class U>
+		struct HasSaturateSub_t<T, U, std::void_t< decltype( std::declval<T&>().SaturateSub(std::declval<U&>()) ) > > : std::true_type {};
+
+		template<class, class, class = std::void_t<>>
+		struct HasSaturateMultiple_t : std::false_type {};
+		template<class T, class U>
+		struct HasSaturateMultiple_t<T, U, std::void_t< decltype( std::declval<T&>().SaturateMultiple(std::declval<U&>()) ) > > : std::true_type {};
+
+		template<class, class, class = std::void_t<>>
+		struct HasSaturateDivide_t : std::false_type {};
+		template<class T, class U>
+		struct HasSaturateDivide_t<T, U, std::void_t< decltype( std::declval<T&>().SaturateDivide(std::declval<U&>()) ) > > : std::true_type {};
+
+		template<class T, class U>
+		struct HasSaturateOperation_t : std::conjunction<HasSaturateAdd_t<T, U>, HasSaturateSub_t<T, U>, HasSaturateMultiple_t<T, U>, HasSaturateDivide_t<T, U>> {};
+
+		template<class, class, class = std::void_t<>>
+		struct HasCheckedAdd_t : std::false_type {};
+		template<class T, class U>
+		struct HasCheckedAdd_t<T, U, std::void_t< decltype( std::declval<T&>().CheckedAdd(std::declval<U&>()) ) > > : std::true_type {};
+
+		template<class, class, class = std::void_t<>>
+		struct HasCheckedSub_t : std::false_type {};
+		template<class T, class U>
+		struct HasCheckedSub_t<T, U, std::void_t< decltype( std::declval<T&>().CheckedSub(std::declval<U&>()) ) > > : std::true_type {};
+
+		template<class, class, class = std::void_t<>>
+		struct HasCheckedMultiple_t : std::false_type {};
+		template<class T, class U>
+		struct HasCheckedMultiple_t<T, U, std::void_t< decltype( std::declval<T&>().CheckedMultiple(std::declval<U&>()) ) > > : std::true_type {};
+
+		template<class, class, class = std::void_t<>>
+		struct HasCheckedDivide_t : std::false_type {};
+		template<class T, class U>
+		struct HasCheckedDivide_t<T, U, std::void_t< decltype( std::declval<T&>().CheckedDivide(std::declval<U&>()) ) > > : std::true_type {};
+
+		template<class T, class U>
+		struct HasCheckedOperation_t : std::conjunction<HasCheckedAdd_t<T, U>, HasCheckedSub_t<T, U>, HasCheckedMultiple_t<T, U>, HasCheckedDivide_t<T, U>> {};
 
 		template<class, class, class = std::void_t<>>
 		struct HasComparamentLarger : std::false_type {};
@@ -81,8 +215,20 @@ namespace zawa_ch::StationaryOrbit
 		struct Equatable_t : std::conjunction< HasEqual<T, U>, HasNotEqual<T, U> > {};
 
 	public:
-		///	基本的な四則演算を持つ型を識別します。
-		template<class T, class U = T> inline constexpr static bool ArithmeticOperatable = ArithmeticOperatable_t<T, U>::value;
+		///	単項算術演算子を持つ型を識別します。
+		template<class T> inline constexpr static bool HasUnaryOperation = HasUnaryOperation_t<T>::value;
+		///	加法演算子を持つ型を識別します。
+		template<class T, class U = T> inline constexpr static bool HasAdditiveOperation = HasAdditiveOperation_t<T, U>::value;
+		///	乗法演算子を持つ型を識別します。
+		template<class T, class U = T> inline constexpr static bool HasMultiplicativeOperation = HasMultiplicativeOperation_t<T, U>::value;
+		///	基本的な算術演算を持つ型を識別します。
+		template<class T, class U = T> inline constexpr static bool HasArithmeticOperation = HasArithmeticOperation_t<T, U>::value;
+		///	基本的なビット演算を持つ型を識別します。
+		template<class T, class U = T> inline constexpr static bool HasBitOperation = HasBitOperation_t<T, U>::value;
+		///	計算結果が飽和する四則演算を持つ型を識別します。
+		template<class T, class U = T> inline constexpr static bool HasSaturateOperation = HasSaturateOperation_t<T, U>::value;
+		///	計算結果の値域チェックが行われる四則演算を持つ型を識別します。
+		template<class T, class U = T> inline constexpr static bool HasCheckedOperation = HasCheckedOperation_t<T, U>::value;
 		///	指定された型のオブジェクトとの比較が可能な型を識別します。
 		template<class T, class U = T> inline constexpr static bool Comparable = Comparable_t<T, U>::value;
 		///	指定された型のオブジェクトとの比較が可能な型を識別します。
--- test/core/traits.cpp
+++ test/core/traits.cpp
@@ -3,26 +3,35 @@
 #include <stdexcept>
 #include <string>
 #include "stationaryorbit/core.traits.hpp"
+#include "stationaryorbit/core.numeral.hpp"
 using namespace zawa_ch::StationaryOrbit;
 
 void Test_Traits()
 {
 	std::cout << "<--- Traits --->" << std::endl;
-	if (Traits::ArithmeticOperatable<int>) { std::cout << "ArithmeticOperatable<int> -> true" << std::endl; } else { throw std::exception(); }
+	if (Traits::HasArithmeticOperation<int>) { std::cout << "HasArithmeticOperation<int> -> true" << std::endl; } else { throw std::exception(); }
+	if (Traits::HasBitOperation<int>) { std::cout << "HasBitOperation<int> -> true" << std::endl; } else { throw std::exception(); }
 	if (Traits::Comparable<int>) { std::cout << "Comparable<int> -> true" << std::endl; } else { throw std::exception(); }
 	if (Traits::Equatable<int>) { std::cout << "Equatable<int> -> true" << std::endl; } else { throw std::exception(); }
 
-	if (Traits::ArithmeticOperatable<double>) { std::cout << "ArithmeticOperatable<double> -> true" << std::endl; } else { throw std::exception(); }
+	if (Traits::HasArithmeticOperation<double>) { std::cout << "HasArithmeticOperation<double> -> true" << std::endl; } else { throw std::exception(); }
+	if (!Traits::HasBitOperation<double>) { std::cout << "HasBitOperation<double> -> false" << std::endl; } else { throw std::exception(); }
 	if (Traits::Comparable<double>) { std::cout << "Comparable<double> -> true" << std::endl; } else { throw std::exception(); }
 	if (Traits::Equatable<double>) { std::cout << "Equatable<double> -> true" << std::endl; } else { throw std::exception(); }
 
-	if (!Traits::ArithmeticOperatable<std::string>) { std::cout << "ArithmeticOperatable<std::string> -> false" << std::endl; } else { throw std::exception(); }
+	if (!Traits::HasArithmeticOperation<std::string>) { std::cout << "HasArithmeticOperation<std::string> -> false" << std::endl; } else { throw std::exception(); }
+	if (!Traits::HasBitOperation<std::string>) { std::cout << "HasBitOperation<std::string> -> false" << std::endl; } else { throw std::exception(); }
 	if (Traits::Comparable<std::string>) { std::cout << "Comparable<std::string> -> true" << std::endl; } else { throw std::exception(); }
 	if (Traits::Equatable<std::string>) { std::cout << "Equatable<std::string> -> true" << std::endl; } else { throw std::exception(); }
 
-	if (BitWidth<uint32_t> == 32UL) { std::cout << "BitWidth<uint32_t> -> 32" << std::endl; } else { throw std::exception(); }
-	if (BitWidth<int64_t> == 64UL) { std::cout << "BitWidth<int64_t> -> 64" << std::endl; } else { throw std::exception(); }
-	if (BitWidth<bool> == 1UL) { std::cout << "BitWidth<bool> -> 1" << std::endl; } else { throw std::exception(); }
-	std::cout << "sizeof(bool) -> " << sizeof(bool) << std::endl;
-	if (BitWidth<std::bitset<14>> == 14UL) { std::cout << "BitWidth<std::bitset<14>> -> 14" << std::endl; } else { throw std::exception(); }
+	if (Traits::HasArithmeticOperation<Proportion8_t>) { std::cout << "HasArithmeticOperation<Proportion8_t> -> true" << std::endl; } else { throw std::exception(); }
+	if (!Traits::HasBitOperation<Proportion8_t>) { std::cout << "HasBitOperation<Proportion8_t> -> false" << std::endl; } else { throw std::exception(); }
+	if (Traits::HasSaturateOperation<Proportion8_t>) { std::cout << "HasSaturateOperation<Proportion8_t> -> true" << std::endl; } else { throw std::exception(); }
+	if (Traits::HasCheckedOperation<Proportion8_t>) { std::cout << "HasCheckedOperation<Proportion8_t> -> true" << std::endl; } else { throw std::exception(); }
+
+	if (BitWidth<uint32_t> == 32UL) { std::cout << "BitWidth<uint32_t> -> " << BitWidth<uint32_t> << "[bit(s)]" << std::endl; } else { throw std::exception(); }
+	if (BitWidth<int64_t> == 64UL) { std::cout << "BitWidth<int64_t> -> " << BitWidth<int64_t> << "[bit(s)]" << std::endl; } else { throw std::exception(); }
+	if (BitWidth<bool> == 1UL) { std::cout << "BitWidth<bool> -> " << BitWidth<bool> << "[bit(s)]" << std::endl; } else { throw std::exception(); }
+	std::cout << "sizeof(bool) -> " << sizeof(bool) << "[byte(s)]" << std::endl;
+	if (BitWidth<std::bitset<14>> == 14UL) { std::cout << "BitWidth<std::bitset<14>> -> " << BitWidth<std::bitset<14>> << "[bit(s)]" << std::endl; } else { throw std::exception(); }
 }
