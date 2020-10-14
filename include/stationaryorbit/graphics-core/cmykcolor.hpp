a4b4365e85f6cf54379b97e874178520a7591eab
zawa-ch <satellite.2e1834097@gmail.com>
2020-10-14 17:12:46 +0900

    * Graphics::CMYKColor
    Graphics::RelativeColorに合わせて変更

--- include/stationaryorbit/graphics-core/cmykcolor.hpp
+++ include/stationaryorbit/graphics-core/cmykcolor.hpp
@@ -18,88 +18,129 @@
 //
 #ifndef __stationaryorbit_graphics_core_cmykcolor__
 #define __stationaryorbit_graphics_core_cmykcolor__
-#include <algorithm>
-#include <cstdint>
-#include "stationaryorbit/core.numeral.hpp"
+#include "relativecolor.hpp"
+#include "translucentcolor.hpp"
 namespace zawa_ch::StationaryOrbit::Graphics
 {
 	///	シアン(Cyan), マゼンタ(Magenta), イエロー(Yellow), キー プレート(Key plate)の四要素によって表される色。
-	template <class Tp = float>
+	template <class Tp>
 	struct CMYKColor final
 	{
 	public: // types
 		///	この型の各チャネルの値の表現のために内部で使用されている型。
-		typedef Proportion<Tp> ChannelType;
-	private:
-		ChannelType _c;
-		ChannelType _m;
-		ChannelType _y;
-		ChannelType _k;
-	public: // construct
+		typedef typename RelativeColor<Tp, 4UL>::ValueType ValueType;
+	private: // contains
+		RelativeColor<Tp, 4UL> _value;
+	public: // constructor
 		///	既定の @a CMYKColor を初期化します。
-		constexpr CMYKColor() : _c(), _m(), _y(), _k() {}
-		///	CMY値を指定して @a CMYKColor を初期化します。
-		constexpr CMYKColor(const ChannelType& c, const ChannelType& m, const ChannelType& y, const ChannelType& k) : _c(c), _m(m), _y(y), _k(k) {}
-		constexpr CMYKColor(const CMYColor<Tp>& color)
-			: _c(color.C() - std::min({color.C(), color.M(), color.Y()}))
-			, _m(color.M() - std::min({color.C(), color.M(), color.Y()}))
-			, _y(color.Y() - std::min({color.C(), color.M(), color.Y()}))
-			, _k(std::min({color.C(), color.M(), color.Y()}))
-		{}
+		constexpr CMYKColor() : _value() {}
+		///	CMYK値を指定して @a CMYKColor を初期化します。
+		constexpr CMYKColor(ValueType c, ValueType m, ValueType y, ValueType k) : _value({c, m, y, k}) {}
+		constexpr explicit CMYKColor(const RelativeColor<Tp, 4UL>& value) : _value(value) {}
 		template <class fromT>
-		constexpr explicit CMYKColor(const CMYKColor<fromT>& from) : CMYKColor(from.template CastTo<Tp>()) {}
+		constexpr explicit CMYKColor(const CMYKColor<fromT>& from) : CMYKColor(RelativeColor<Tp, 4UL>(from.Data())) {}
+		constexpr CMYKColor(const ZeroValue_t&) : _value(Zero) {}
 	public: // member
+		constexpr const RelativeColor<Tp, 3U>& Data() const { return _value; }
 		///	この @a CMYKColor のシアンのコンポーネントを取得します。
-		constexpr const ChannelType& C() const { return _c; }
+		constexpr const ValueType& C() const { return _value.Data()[0]; }
 		///	この @a CMYKColor のマゼンタのコンポーネントを取得します。
-		constexpr const ChannelType& M() const { return _m; }
+		constexpr const ValueType& M() const { return _value.Data()[1]; }
 		///	この @a CMYKColor のイエローのコンポーネントを取得します。
-		constexpr const ChannelType& Y() const { return _y; }
+		constexpr const ValueType& Y() const { return _value.Data()[2]; }
 		///	この @a CMYKColor のキープレート(黒色)のコンポーネントを取得します。
-		constexpr const ChannelType& K() const { return _k; }
+		constexpr const ValueType& K() const { return _value.Data()[3]; }
 		///	この @a CMYKColor が正規化されているかを取得します。
-		constexpr bool IsNormalized() const { return (_c.IsNormalized())&&(_m.IsNormalized())&&(_y.IsNormalized())&&(_k.IsNormalized()); }
-		///	この @a CMYKColor が空であるかを取得します。
-		constexpr bool IsEmpty() const { return *this == Empty(); }
+		constexpr bool IsNormalized() const { return _value.IsNormalized(); }
+		///	正規化した @a CMYKColor を取得します。
+		constexpr CMYKColor<Tp> Normalize() const { return CMYKColor<Tp>(_value.Normalize()); }
 
-		constexpr CMYKColor<Tp> operator+(const CMYKColor<Tp>& other) const { return CMYKColor<Tp>(_c + other._c, _m + other._m, _y + other._y, _k + other._k); }
-		constexpr CMYKColor<Tp> operator-(const CMYKColor<Tp>& other) const { return CMYKColor<Tp>(_c - other._c, _m - other._m, _y - other._y, _k - other._k); }
-		constexpr CMYKColor<Tp> operator*(const CMYKColor<Tp>& other) const { return CMYKColor<Tp>(_c * other._c, _m * other._m, _y * other._y, _k * other._k); }
-		constexpr CMYKColor<Tp> operator*(const ChannelType& other) const { return CMYKColor<Tp>(_c * other, _m * other, _y * other, _k * other); }
-		constexpr CMYKColor<Tp> operator/(const CMYKColor<Tp>& other) const { return CMYKColor<Tp>(_c / other._c, _m / other._m, _y / other._y, _k / other._k); }
-		constexpr CMYKColor<Tp> operator/(const ChannelType& other) const { return CMYKColor<Tp>(_c / other, _m / other, _y / other, _k / other); }
-		constexpr CMYKColor<Tp> operator~() const { return CMYKColor<Tp>(ChannelType::Max()-_c, ChannelType::Max()-_m, ChannelType::Max()-_y, ChannelType::Max()-_k); }
-		constexpr CMYKColor<Tp> operator|(const CMYKColor<Tp>& other) const { return CMYKColor<Tp>(((other._c < _c)?(_c):(other._c)), ((other._m < _m)?(_m):(other._m)), ((other._y < _y)?(_y):(other._y)), ((other._k < _k)?(_k):(other._k))); }
-		constexpr CMYKColor<Tp> operator&(const CMYKColor<Tp>& other) const { return CMYKColor<Tp>(((_c < other._c)?(_c):(other._c)), ((_m < other._m)?(_m):(other._m)), ((_y < other._y)?(_y):(other._y)), ((_k < other._k)?(_k):(other._k))); }
-		constexpr CMYKColor<Tp> operator^(const CMYKColor<Tp>& other) const { return (*this | other) & ~(*this & other); }
+		[[nodiscard]] constexpr CMYKColor<Tp> Add(const CMYKColor<Tp>& other) const noexcept { return CMYKColor<Tp>(_value.Add(other._value)); }
+		[[nodiscard]] constexpr CMYKColor<Tp> Subtract(const CMYKColor<Tp>& other) const noexcept { return CMYKColor<Tp>(_value.Subtract(other._value)); }
+		[[nodiscard]] constexpr CMYKColor<Tp> Multiply(const CMYKColor<Tp>& other) const noexcept { return CMYKColor<Tp>(_value.Multiply(other._value)); }
+		[[nodiscard]] constexpr CMYKColor<Tp> Multiply(const ValueType& other) const noexcept { return CMYKColor<Tp>(_value.Multiply(other._value)); }
+		[[nodiscard]] constexpr CMYKColor<Tp> Divide(const CMYKColor<Tp>& other) const noexcept { return CMYKColor<Tp>(_value.Divide(other._value)); }
+		[[nodiscard]] constexpr CMYKColor<Tp> Divide(const ValueType& other) const noexcept { return CMYKColor<Tp>(_value.Divide(other._value)); }
+		[[nodiscard]] constexpr CMYKColor<Tp> SaturateAdd(const CMYKColor<Tp>& other) const noexcept { return CMYKColor<Tp>(_value.SaturateAdd(other._value)); }
+		[[nodiscard]] constexpr CMYKColor<Tp> SaturateSubtract(const CMYKColor<Tp>& other) const noexcept { return CMYKColor<Tp>(_value.SaturateSubtract(other._value)); }
+		[[nodiscard]] constexpr CMYKColor<Tp> SaturateMultiply(const CMYKColor<Tp>& other) const noexcept { return CMYKColor<Tp>(_value.SaturateMultiply(other._value)); }
+		[[nodiscard]] constexpr CMYKColor<Tp> SaturateMultiply(const ValueType& other) const noexcept { return CMYKColor<Tp>(_value.SaturateMultiply(other._value)); }
+		[[nodiscard]] constexpr CMYKColor<Tp> SaturateDivide(const CMYKColor<Tp>& other) const noexcept { return CMYKColor<Tp>(_value.SaturateDivide(other._value)); }
+		[[nodiscard]] constexpr CMYKColor<Tp> SaturateDivide(const ValueType& other) const noexcept { return CMYKColor<Tp>(_value.SaturateDivide(other._value)); }
+		[[nodiscard]] constexpr CMYKColor<Tp> CheckedAdd(const CMYKColor<Tp>& other) const { return CMYKColor<Tp>(_value.CheckedAdd(other._value)); }
+		[[nodiscard]] constexpr CMYKColor<Tp> CheckedSubtract(const CMYKColor<Tp>& other) const { return CMYKColor<Tp>(_value.CheckedSubtract(other._value)); }
+		[[nodiscard]] constexpr CMYKColor<Tp> CheckedMultiply(const CMYKColor<Tp>& other) const { return CMYKColor<Tp>(_value.CheckedMultiply(other._value)); }
+		[[nodiscard]] constexpr CMYKColor<Tp> CheckedMultiply(const ValueType& other) const { return CMYKColor<Tp>(_value.CheckedMultiply(other._value)); }
+		[[nodiscard]] constexpr CMYKColor<Tp> CheckedDivide(const CMYKColor<Tp>& other) const { return CMYKColor<Tp>(_value.CheckedDivide(other._value)); }
+		[[nodiscard]] constexpr CMYKColor<Tp> CheckedDivide(const ValueType& other) const { return CMYKColor<Tp>(_value.CheckedDivide(other._value)); }
+		[[nodiscard]] constexpr CMYKColor<Tp> And(const CMYKColor<Tp>& other) const noexcept { return CMYKColor<Tp>(_value.And(other._value)); }
+		[[nodiscard]] constexpr CMYKColor<Tp> Or(const CMYKColor<Tp>& other) const noexcept { return CMYKColor<Tp>(_value.Or(other._value)); }
+		[[nodiscard]] constexpr CMYKColor<Tp> Not() const noexcept { return CMYKColor<Tp>(_value.Not()); }
+		[[nodiscard]] constexpr CMYKColor<Tp> Xor(const CMYKColor<Tp>& other) const noexcept { return CMYKColor<Tp>(_value.Xor(other._value)); }
+
+		constexpr CMYKColor<Tp> operator+(const CMYKColor<Tp>& other) const { return Add(other); }
+		constexpr CMYKColor<Tp> operator-(const CMYKColor<Tp>& other) const { return Subtract(other); }
+		constexpr CMYKColor<Tp> operator*(const CMYKColor<Tp>& other) const { return Multiply(other); }
+		constexpr CMYKColor<Tp> operator*(const ValueType& other) const { return Multiply(other); }
+		constexpr CMYKColor<Tp> operator/(const CMYKColor<Tp>& other) const { return Divide(other); }
+		constexpr CMYKColor<Tp> operator/(const ValueType& other) const { return Divide(other); }
+		constexpr CMYKColor<Tp> operator~() const { return Not(); }
+		constexpr CMYKColor<Tp> operator|(const CMYKColor<Tp>& other) const { return Or(other); }
+		constexpr CMYKColor<Tp> operator&(const CMYKColor<Tp>& other) const { return And(other); }
+		constexpr CMYKColor<Tp> operator^(const CMYKColor<Tp>& other) const { return Xor(other); }
 		constexpr CMYKColor<Tp>& operator+=(const CMYKColor<Tp>& other) { return *this = *this + other; }
 		constexpr CMYKColor<Tp>& operator-=(const CMYKColor<Tp>& other) { return *this = *this - other; }
 		constexpr CMYKColor<Tp>& operator*=(const CMYKColor<Tp>& other) { return *this = *this * other; }
-		constexpr CMYKColor<Tp>& operator*=(const ChannelType& other) { return *this = *this * other; }
+		constexpr CMYKColor<Tp>& operator*=(const ValueType& other) { return *this = *this * other; }
 		constexpr CMYKColor<Tp>& operator/=(const CMYKColor<Tp>& other) { return *this = *this / other; }
-		constexpr CMYKColor<Tp>& operator/=(const ChannelType& other) { return *this = *this / other; }
+		constexpr CMYKColor<Tp>& operator/=(const ValueType& other) { return *this = *this / other; }
 		constexpr CMYKColor<Tp>& operator|=(const CMYKColor<Tp>& other) { return *this = *this | other; }
 		constexpr CMYKColor<Tp>& operator&=(const CMYKColor<Tp>& other) { return *this = *this & other; }
 		constexpr CMYKColor<Tp>& operator^=(const CMYKColor<Tp>& other) { return *this = *this ^ other; }
 
-		///	正規化した @a CMYKColor を取得します。
-		constexpr CMYKColor<Tp> Normalize() const { return CMYKColor<Tp>(_c.Normalize(), _m.Normalize(), _y.Normalize(), _k.Normalize()); }
-
-		template <class castT>
-		constexpr CMYKColor<castT> CastTo() const { return CMYKColor<castT>(_c.template CastTo<castT>(), _m.template CastTo<castT>(), _y.template CastTo<castT>(), _k.template CastTo<castT>()); }
-		constexpr explicit operator CMYColor<Tp>() const { return CMYColor<Tp>(_k + _c, _k + _m, _k + _y); }
-
-		///	指定されたオブジェクトがこのオブジェクトと等価であることをテストします。
-		constexpr bool Equals(const CMYKColor<Tp>& other) const { return (_c == other._c)&&(_m == other._m)&&(_y == other._y); }
+		constexpr bool Equals(const CMYKColor<Tp>& other) const { return _value.Equals(other._value); }
 		constexpr bool operator==(const CMYKColor<Tp>& other) const { return Equals(other); }
 		constexpr bool operator!=(const CMYKColor<Tp>& other) const { return !Equals(other); }
 
 		constexpr static CMYKColor<Tp> Empty() { return CMYKColor<Tp>(); }
 	};
 
-	typedef CMYKColor<uint8_t> CMYKI8_t;
-	typedef CMYKColor<uint16_t> CMYKI16_t;
+	extern template struct CMYKColor<Proportion8_t>;
+	extern template struct CMYKColor<Proportion16_t>;
+	extern template struct CMYKColor<Proportion32_t>;
+	extern template struct CMYKColor<Proportion64_t>;
+	extern template struct CMYKColor<FixedPoint16q15_t>;
+	extern template struct CMYKColor<FixedPoint32q31_t>;
+	extern template struct CMYKColor<FixedPoint64q63_t>;
+	extern template struct CMYKColor<float>;
+	extern template struct CMYKColor<double>;
+	extern template struct TranslucentColor<CMYKColor<Proportion8_t>>;
+	extern template struct TranslucentColor<CMYKColor<Proportion16_t>>;
+	extern template struct TranslucentColor<CMYKColor<Proportion32_t>>;
+	extern template struct TranslucentColor<CMYKColor<Proportion64_t>>;
+	extern template struct TranslucentColor<CMYKColor<FixedPoint16q15_t>>;
+	extern template struct TranslucentColor<CMYKColor<FixedPoint32q31_t>>;
+	extern template struct TranslucentColor<CMYKColor<FixedPoint64q63_t>>;
+	extern template struct TranslucentColor<CMYKColor<float>>;
+	extern template struct TranslucentColor<CMYKColor<double>>;
+
+	typedef CMYKColor<Proportion8_t> CMYK8_t;
+	typedef CMYKColor<Proportion16_t> CMYK16_t;
+	typedef CMYKColor<Proportion32_t> CMYK32_t;
+	typedef CMYKColor<Proportion64_t> CMYK64_t;
+	typedef CMYKColor<FixedPoint16q15_t> CMYKI16_t;
+	typedef CMYKColor<FixedPoint32q31_t> CMYKI32_t;
+	typedef CMYKColor<FixedPoint64q63_t> CMYKI64_t;
 	typedef CMYKColor<float> CMYKF32_t;
 	typedef CMYKColor<double> CMYKF64_t;
+	typedef TranslucentColor<CMYKColor<Proportion8_t>> ACMYK8_t;
+	typedef TranslucentColor<CMYKColor<Proportion16_t>> ACMYK16_t;
+	typedef TranslucentColor<CMYKColor<Proportion32_t>> ACMYK32_t;
+	typedef TranslucentColor<CMYKColor<Proportion64_t>> ACMYK64_t;
+	typedef TranslucentColor<CMYKColor<FixedPoint16q15_t>> ACMYKI16_t;
+	typedef TranslucentColor<CMYKColor<FixedPoint32q31_t>> ACMYKI32_t;
+	typedef TranslucentColor<CMYKColor<FixedPoint64q63_t>> ACMYKI64_t;
+	typedef TranslucentColor<CMYKColor<float>> ACMYKF32_t;
+	typedef TranslucentColor<CMYKColor<double>> ACMYKF64_t;
 }
 #endif // __stationaryorbit_graphics_core_cmykcolor__
