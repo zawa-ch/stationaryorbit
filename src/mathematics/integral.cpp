e4427729472261330f0223024f399cd01c4c8669
zawa-ch <satellite.2e1834097@gmail.com>
2020-10-14 17:23:00 +0900

    * Mathematics::Integral
    Range修正に対応

--- src/mathematics/integral.cpp
+++ src/mathematics/integral.cpp
@@ -26,24 +26,24 @@ MidpointIntegral::MidpointIntegral(const IMathematicFunction<double>& function, 
 {
 	if (
 		!(
-			(std::numeric_limits<double>::lowest() <= integrationrange.getFloor())
-			&&(integrationrange.getFloor() <= std::numeric_limits<double>::max())
-			&&(std::numeric_limits<double>::lowest() <= integrationrange.getCailing())
-			&&(integrationrange.getCailing() <= std::numeric_limits<double>::max())
+			(std::numeric_limits<double>::lowest() <= integrationrange.GetFloor())
+			&&(integrationrange.GetFloor() <= std::numeric_limits<double>::max())
+			&&(std::numeric_limits<double>::lowest() <= integrationrange.GetCeiling())
+			&&(integrationrange.GetCeiling() <= std::numeric_limits<double>::max())
 		)
 	)
 	{ throw std::invalid_argument("引数 integrationrange として指定可能な範囲は有限な実数である必要があります。"); }
 }
 const IMathematicFunction<double>& MidpointIntegral::Function() const { return _func; }
 Range<double, true, true> MidpointIntegral::getIntegrationRange() const { return _irange; }
-double MidpointIntegral::Calc(const double& value) const { return _irange.Length() * _func.Calc((_irange.getFloor() + _irange.getCailing()) / 2); }
+double MidpointIntegral::Calc(const double& value) const { return _irange.Length() * _func.Calc((_irange.GetFloor() + _irange.GetCeiling()) / 2); }
 double MidpointIntegral::Calc(const double& value, size_t div) const
 {
 	auto result = CompensatedDouble(0.0);
 	auto length = _irange.Length();
 	for (size_t i = 0; i < div; i++)
 	{
-		result += _func.Calc(_irange.getFloor() + (((length * i) + (length * (i + 1)) / div / 2)));
+		result += _func.Calc(_irange.GetFloor() + (((length * i) + (length * (i + 1)) / div / 2)));
 	}
 	return result.getValue();
 }
@@ -53,17 +53,17 @@ TrapeziumIntegral::TrapeziumIntegral(const IMathematicFunction<double>& function
 {
 	if (
 		!(
-			(std::numeric_limits<double>::lowest() <= integrationrange.getFloor())
-			&&(integrationrange.getFloor() <= std::numeric_limits<double>::max())
-			&&(std::numeric_limits<double>::lowest() <= integrationrange.getCailing())
-			&&(integrationrange.getCailing() <= std::numeric_limits<double>::max())
+			(std::numeric_limits<double>::lowest() <= integrationrange.GetFloor())
+			&&(integrationrange.GetFloor() <= std::numeric_limits<double>::max())
+			&&(std::numeric_limits<double>::lowest() <= integrationrange.GetCeiling())
+			&&(integrationrange.GetCeiling() <= std::numeric_limits<double>::max())
 		)
 	)
 	{ throw std::invalid_argument("引数 integrationrange として指定可能な範囲は有限な実数である必要があります。"); }
 }
 const IMathematicFunction<double>& TrapeziumIntegral::Function() const { return _func; }
 Range<double, true, true> TrapeziumIntegral::getIntegrationRange() const { return _irange; }
-double TrapeziumIntegral::Calc(const double& value) const { return (_func.Calc(_irange.getFloor()) + _func.Calc(_irange.getCailing())) * _irange.Length() / 2; }
+double TrapeziumIntegral::Calc(const double& value) const { return (_func.Calc(_irange.GetFloor()) + _func.Calc(_irange.GetCeiling())) * _irange.Length() / 2; }
 double TrapeziumIntegral::Calc(const double& value, size_t div) const
 {
 	auto result = CompensatedDouble(0.0);
@@ -80,10 +80,10 @@ SimpsonIntegral::SimpsonIntegral(const IMathematicFunction<double>& function, co
 {
 	if (
 		!(
-			(std::numeric_limits<double>::lowest() <= integrationrange.getFloor())
-			&&(integrationrange.getFloor() <= std::numeric_limits<double>::max())
-			&&(std::numeric_limits<double>::lowest() <= integrationrange.getCailing())
-			&&(integrationrange.getCailing() <= std::numeric_limits<double>::max())
+			(std::numeric_limits<double>::lowest() <= integrationrange.GetFloor())
+			&&(integrationrange.GetFloor() <= std::numeric_limits<double>::max())
+			&&(std::numeric_limits<double>::lowest() <= integrationrange.GetCeiling())
+			&&(integrationrange.GetCeiling() <= std::numeric_limits<double>::max())
 		)
 	)
 	{ throw std::invalid_argument("引数 integrationrange として指定可能な範囲は有限な実数である必要があります。"); }
