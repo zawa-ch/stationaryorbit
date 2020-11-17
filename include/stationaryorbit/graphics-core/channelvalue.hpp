97514425413c6273652ab3209f877a4ee5ba5f48
zawa-ch <satellite.2e1834097@gmail.com>
2020-11-17 14:53:56 +0900

    * Graphics::ChannelValue

--- include/stationaryorbit/graphics-core/channelvalue.hpp
+++ include/stationaryorbit/graphics-core/channelvalue.hpp
@@ -22,6 +22,9 @@
 #include "stationaryorbit/core.numeral.hpp"
 namespace zawa_ch::StationaryOrbit::Graphics
 {
+	///	0..1の範囲で表される色チャネルを表します。
+	///	@param	Tp
+	///	値の表現に用いるための型。
 	template<class Tp, class = void>
 	struct ChannelValue final
 	{
@@ -39,18 +42,19 @@ namespace zawa_ch::StationaryOrbit::Graphics
 		ValueType _value;
 
 	public:
-		constexpr ChannelValue() noexcept = default;
+		constexpr ChannelValue() noexcept : _value() {}
 		constexpr ChannelValue(const ValueType& value) noexcept : _value(value) {}
 		template<class fromT>
 		constexpr explicit ChannelValue(const ChannelValue<fromT>& from) : _value(static_cast<ValueType>(from.Data())) {}
 		constexpr ChannelValue(const ZeroValue_t&) noexcept : _value(Zero) {}
-		constexpr ChannelValue(const ChannelValue<Tp>&) = default;
-		constexpr ChannelValue(ChannelValue<Tp>&&) = default;
 		~ChannelValue() = default;
 
 	public:
+		///	このオブジェクトの内部用いられているデータを取得します。
 		[[nodiscard]] constexpr const ValueType& Data() const noexcept { return _value; }
+		///	このオブジェクトが正規化された値であるかを取得します。
 		[[nodiscard]] constexpr bool IsNormalized() const noexcept { return (Min()._value <= _value)&&(_value <= Max()._value); }
+		///	正規化されたオブジェクトを取得します。
 		[[nodiscard]] constexpr ChannelValue<Tp> Normalize() const noexcept { return (Min()._value <= _value)?((_value <= Max()._value)?_value:Max()._value):(Min()._value); }
 
 		[[nodiscard]] constexpr ChannelValue<Tp> Promote() const noexcept { return ChannelValue<Tp>(+_value); }
@@ -100,15 +104,18 @@ namespace zawa_ch::StationaryOrbit::Graphics
 		[[nodiscard]] constexpr bool operator>(const ChannelValue<Tp>& other) const noexcept { return Compare(other) > 0; }
 		[[nodiscard]] constexpr bool operator<=(const ChannelValue<Tp>& other) const noexcept { return Compare(other) <= 0; }
 		[[nodiscard]] constexpr bool operator>=(const ChannelValue<Tp>& other) const noexcept { return Compare(other) >= 0; }
-		constexpr ChannelValue<Tp>& operator=(const ChannelValue<Tp>&) = default;
-		constexpr ChannelValue<Tp>& operator=(ChannelValue<Tp>&&) = default;
 
 	public:
+		///	空のオブジェクトを取得します。
 		[[nodiscard]] constexpr static ChannelValue<Tp> Empty() noexcept { return ChannelValue<Tp>(); }
+		///	この型で表せる最大の値を持つオブジェクトを取得します。
 		[[nodiscard]] constexpr static ChannelValue<Tp> Max() noexcept { return ChannelValue<Tp>(ValueType(1)); }
+		///	この型で表せる最小の値を持つオブジェクトを取得します。
 		[[nodiscard]] constexpr static ChannelValue<Tp> Min() noexcept { return ChannelValue<Tp>(ValueType(0)); }
 	};
 
+	///	0..1の範囲で表される色チャネルを表します。
+	///	@a ChannelValue の浮動小数点型に対する特殊化です。
 	template<class Tp>
 	struct ChannelValue<Tp, std::enable_if_t<std::is_floating_point_v<Tp>>>
 	{
@@ -129,13 +136,14 @@ namespace zawa_ch::StationaryOrbit::Graphics
 		template<class fromT>
 		constexpr explicit ChannelValue(const ChannelValue<fromT>& from) : _value(static_cast<ValueType>(from.Data())) {}
 		constexpr ChannelValue(const ZeroValue_t&) : _value(Zero) {}
-		constexpr ChannelValue(const ChannelValue<Tp>&) = default;
-		constexpr ChannelValue(ChannelValue<Tp>&&) = default;
 		~ChannelValue() = default;
 
 	public:
+		///	このオブジェクトの内部用いられているデータを取得します。
 		[[nodiscard]] constexpr ValueType Data() const noexcept { return _value; }
+		///	このオブジェクトが正規化された値であるかを取得します。
 		[[nodiscard]] constexpr bool IsNormalized() const noexcept { return (Min()._value <= _value)&&(_value <= Max()._value); }
+		///	正規化されたオブジェクトを取得します。
 		[[nodiscard]] constexpr ChannelValue<Tp> Normalize() const noexcept { return (Min()._value <= _value)?((_value <= Max()._value)?_value:Max()._value):(Min()._value); }
 
 		[[nodiscard]] constexpr ChannelValue<Tp> Promote() const noexcept { return ChannelValue<Tp>(+_value); }
@@ -185,12 +193,13 @@ namespace zawa_ch::StationaryOrbit::Graphics
 		[[nodiscard]] constexpr bool operator>(const ChannelValue<Tp>& other) const noexcept { return Compare(other) > 0; }
 		[[nodiscard]] constexpr bool operator<=(const ChannelValue<Tp>& other) const noexcept { return Compare(other) <= 0; }
 		[[nodiscard]] constexpr bool operator>=(const ChannelValue<Tp>& other) const noexcept { return Compare(other) >= 0; }
-		constexpr ChannelValue<Tp>& operator=(const ChannelValue<Tp>&) = default;
-		constexpr ChannelValue<Tp>& operator=(ChannelValue<Tp>&&) = default;
 
 	public:
+		///	空のオブジェクトを取得します。
 		[[nodiscard]] constexpr static ChannelValue<Tp> Empty() noexcept { return ChannelValue<Tp>(); }
+		///	この型で表せる最大の値を持つオブジェクトを取得します。
 		[[nodiscard]] constexpr static ChannelValue<Tp> Max() noexcept { return ChannelValue<Tp>(ValueType(1)); }
+		///	この型で表せる最小の値を持つオブジェクトを取得します。
 		[[nodiscard]] constexpr static ChannelValue<Tp> Min() noexcept { return ChannelValue<Tp>(ValueType(0)); }
 	};
 
@@ -204,14 +213,23 @@ namespace zawa_ch::StationaryOrbit::Graphics
 	extern template struct ChannelValue<float>;
 	extern template struct ChannelValue<double>;
 
+	///	内部に @a Proportion8_t 型のデータを持つ @a ChannelValue です。
 	typedef ChannelValue<Proportion8_t> Channel8_t;
+	/// 内部に @a Proportion16_t 型のデータを持つ @a ChannelValue です。
 	typedef ChannelValue<Proportion16_t> Channel16_t;
+	/// 内部に @a Proportion32_t 型のデータを持つ @a ChannelValue です。
 	typedef ChannelValue<Proportion32_t> Channel32_t;
+	/// 内部に @a Proportion64_t 型のデータを持つ @a ChannelValue です。
 	typedef ChannelValue<Proportion64_t> Channel64_t;
+	/// 内部に @a FixedPoint16q15_t 型のデータを持つ @a ChannelValue です。
 	typedef ChannelValue<FixedPoint16q15_t> ChannelI16_t;
+	/// 内部に @a FixedPoint32q31_t 型のデータを持つ @a ChannelValue です。
 	typedef ChannelValue<FixedPoint32q31_t> ChannelI32_t;
+	/// 内部に @a FixedPoint64q63_t 型のデータを持つ @a ChannelValue です。
 	typedef ChannelValue<FixedPoint64q63_t> ChannelI64_t;
+	/// 内部に @a float 型のデータを持つ @a ChannelValue です。
 	typedef ChannelValue<float> ChannelF32_t;
+	/// 内部に @a double 型のデータを持つ @a ChannelValue です。
 	typedef ChannelValue<double> ChannelF64_t;
 }
 #endif // __stationaryorbit_graphics_core_channelvalue__
