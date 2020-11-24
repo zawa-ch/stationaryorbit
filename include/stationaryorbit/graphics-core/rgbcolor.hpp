2af7b2aba140f22ceb433bd02e4aa933b1492344
zawa-ch <satellite.2e1834097@gmail.com>
2020-11-24 18:40:05 +0900

    * Graphics::RGBColor
    ビルドパス

--- include/stationaryorbit/graphics-core/rgbcolor.hpp
+++ include/stationaryorbit/graphics-core/rgbcolor.hpp
@@ -27,21 +27,22 @@ namespace zawa_ch::StationaryOrbit::Graphics
 	struct RGBColor final
 	{
 	public: // types
+		typedef RelativeColor<Tp, 3UL> DataType;
 		///	この型の各チャネルの値の表現のために内部で使用されている型。
-		typedef typename RelativeColor<Tp, 3UL>::ValueType ValueType;
+		typedef typename DataType::ValueType ValueType;
 	private: // contains
-		RelativeColor<Tp, 3UL> _value;
+		DataType _value;
 	public: // constructor
 		///	既定の @a RGBColor を初期化します。
-		constexpr RGBColor() : _value() {}
+		constexpr RGBColor() = default;
 		///	RGB値を指定して @a RGBColor を初期化します。
 		constexpr RGBColor(const ValueType& r, const ValueType& g, const ValueType& b) : _value({r, g, b}) {}
-		constexpr explicit RGBColor(const RelativeColor<Tp, 3UL>& value) : _value(value) {}
+		constexpr explicit RGBColor(const DataType& value) : _value(value) {}
 		template <class fromT>
-		constexpr explicit RGBColor(const RGBColor<fromT>& from) : RGBColor(RelativeColor<Tp, 3UL>(from.Data())) {}
+		constexpr explicit RGBColor(const RGBColor<fromT>& from) : RGBColor(DataType(from.Data())) {}
 		constexpr RGBColor(const ZeroValue_t&) : _value(Zero) {}
 	public: // member
-		constexpr const RelativeColor<Tp, 3U>& Data() const { return _value; }
+		constexpr const DataType& Data() const { return _value; }
 		///	この @a RGBColor の赤要素を取得します。
 		constexpr const ValueType& R() const { return _value.Data()[0]; }
 		///	この @a RGBColor の緑要素を取得します。
@@ -56,21 +57,21 @@ namespace zawa_ch::StationaryOrbit::Graphics
 		[[nodiscard]] constexpr RGBColor<Tp> Add(const RGBColor<Tp>& other) const noexcept { return RGBColor<Tp>(_value.Add(other._value)); }
 		[[nodiscard]] constexpr RGBColor<Tp> Subtract(const RGBColor<Tp>& other) const noexcept { return RGBColor<Tp>(_value.Subtract(other._value)); }
 		[[nodiscard]] constexpr RGBColor<Tp> Multiply(const RGBColor<Tp>& other) const noexcept { return RGBColor<Tp>(_value.Multiply(other._value)); }
-		[[nodiscard]] constexpr RGBColor<Tp> Multiply(const ValueType& other) const noexcept { return RGBColor<Tp>(_value.Multiply(other._value)); }
+		[[nodiscard]] constexpr RGBColor<Tp> Multiply(const ValueType& other) const noexcept { return RGBColor<Tp>(_value.Multiply(other.Data())); }
 		[[nodiscard]] constexpr RGBColor<Tp> Divide(const RGBColor<Tp>& other) const noexcept { return RGBColor<Tp>(_value.Divide(other._value)); }
-		[[nodiscard]] constexpr RGBColor<Tp> Divide(const ValueType& other) const noexcept { return RGBColor<Tp>(_value.Divide(other._value)); }
+		[[nodiscard]] constexpr RGBColor<Tp> Divide(const ValueType& other) const noexcept { return RGBColor<Tp>(_value.Divide(other.Data())); }
 		[[nodiscard]] constexpr RGBColor<Tp> SaturateAdd(const RGBColor<Tp>& other) const noexcept { return RGBColor<Tp>(_value.SaturateAdd(other._value)); }
 		[[nodiscard]] constexpr RGBColor<Tp> SaturateSubtract(const RGBColor<Tp>& other) const noexcept { return RGBColor<Tp>(_value.SaturateSubtract(other._value)); }
 		[[nodiscard]] constexpr RGBColor<Tp> SaturateMultiply(const RGBColor<Tp>& other) const noexcept { return RGBColor<Tp>(_value.SaturateMultiply(other._value)); }
-		[[nodiscard]] constexpr RGBColor<Tp> SaturateMultiply(const ValueType& other) const noexcept { return RGBColor<Tp>(_value.SaturateMultiply(other._value)); }
+		[[nodiscard]] constexpr RGBColor<Tp> SaturateMultiply(const ValueType& other) const noexcept { return RGBColor<Tp>(_value.SaturateMultiply(other.Data())); }
 		[[nodiscard]] constexpr RGBColor<Tp> SaturateDivide(const RGBColor<Tp>& other) const noexcept { return RGBColor<Tp>(_value.SaturateDivide(other._value)); }
-		[[nodiscard]] constexpr RGBColor<Tp> SaturateDivide(const ValueType& other) const noexcept { return RGBColor<Tp>(_value.SaturateDivide(other._value)); }
+		[[nodiscard]] constexpr RGBColor<Tp> SaturateDivide(const ValueType& other) const noexcept { return RGBColor<Tp>(_value.SaturateDivide(other.Data())); }
 		[[nodiscard]] constexpr RGBColor<Tp> CheckedAdd(const RGBColor<Tp>& other) const { return RGBColor<Tp>(_value.CheckedAdd(other._value)); }
 		[[nodiscard]] constexpr RGBColor<Tp> CheckedSubtract(const RGBColor<Tp>& other) const { return RGBColor<Tp>(_value.CheckedSubtract(other._value)); }
 		[[nodiscard]] constexpr RGBColor<Tp> CheckedMultiply(const RGBColor<Tp>& other) const { return RGBColor<Tp>(_value.CheckedMultiply(other._value)); }
-		[[nodiscard]] constexpr RGBColor<Tp> CheckedMultiply(const ValueType& other) const { return RGBColor<Tp>(_value.CheckedMultiply(other._value)); }
+		[[nodiscard]] constexpr RGBColor<Tp> CheckedMultiply(const ValueType& other) const { return RGBColor<Tp>(_value.CheckedMultiply(other.Data())); }
 		[[nodiscard]] constexpr RGBColor<Tp> CheckedDivide(const RGBColor<Tp>& other) const { return RGBColor<Tp>(_value.CheckedDivide(other._value)); }
-		[[nodiscard]] constexpr RGBColor<Tp> CheckedDivide(const ValueType& other) const { return RGBColor<Tp>(_value.CheckedDivide(other._value)); }
+		[[nodiscard]] constexpr RGBColor<Tp> CheckedDivide(const ValueType& other) const { return RGBColor<Tp>(_value.CheckedDivide(other.Data())); }
 		[[nodiscard]] constexpr RGBColor<Tp> And(const RGBColor<Tp>& other) const noexcept { return RGBColor<Tp>(_value.And(other._value)); }
 		[[nodiscard]] constexpr RGBColor<Tp> Or(const RGBColor<Tp>& other) const noexcept { return RGBColor<Tp>(_value.Or(other._value)); }
 		[[nodiscard]] constexpr RGBColor<Tp> Not() const noexcept { return RGBColor<Tp>(_value.Not()); }
--- src/graphics-core/rgbcolor.cpp
+++ src/graphics-core/rgbcolor.cpp
@@ -18,21 +18,39 @@
 //
 #include "stationaryorbit/graphics-core/rgbcolor.hpp"
 
-extern template struct zawa_ch::StationaryOrbit::Graphics::RGBColor<zawa_ch::StationaryOrbit::Proportion8_t>;
-extern template struct zawa_ch::StationaryOrbit::Graphics::RGBColor<zawa_ch::StationaryOrbit::Proportion16_t>;
-extern template struct zawa_ch::StationaryOrbit::Graphics::RGBColor<zawa_ch::StationaryOrbit::Proportion32_t>;
-extern template struct zawa_ch::StationaryOrbit::Graphics::RGBColor<zawa_ch::StationaryOrbit::Proportion64_t>;
-extern template struct zawa_ch::StationaryOrbit::Graphics::RGBColor<zawa_ch::StationaryOrbit::FixedPoint16q15_t>;
-extern template struct zawa_ch::StationaryOrbit::Graphics::RGBColor<zawa_ch::StationaryOrbit::FixedPoint32q31_t>;
-extern template struct zawa_ch::StationaryOrbit::Graphics::RGBColor<zawa_ch::StationaryOrbit::FixedPoint64q63_t>;
-extern template struct zawa_ch::StationaryOrbit::Graphics::RGBColor<float>;
-extern template struct zawa_ch::StationaryOrbit::Graphics::RGBColor<double>;
-extern template struct zawa_ch::StationaryOrbit::Graphics::TranslucentColor<zawa_ch::StationaryOrbit::Graphics::RGBColor<zawa_ch::StationaryOrbit::Proportion8_t>>;
-extern template struct zawa_ch::StationaryOrbit::Graphics::TranslucentColor<zawa_ch::StationaryOrbit::Graphics::RGBColor<zawa_ch::StationaryOrbit::Proportion16_t>>;
-extern template struct zawa_ch::StationaryOrbit::Graphics::TranslucentColor<zawa_ch::StationaryOrbit::Graphics::RGBColor<zawa_ch::StationaryOrbit::Proportion32_t>>;
-extern template struct zawa_ch::StationaryOrbit::Graphics::TranslucentColor<zawa_ch::StationaryOrbit::Graphics::RGBColor<zawa_ch::StationaryOrbit::Proportion64_t>>;
-extern template struct zawa_ch::StationaryOrbit::Graphics::TranslucentColor<zawa_ch::StationaryOrbit::Graphics::RGBColor<zawa_ch::StationaryOrbit::FixedPoint16q15_t>>;
-extern template struct zawa_ch::StationaryOrbit::Graphics::TranslucentColor<zawa_ch::StationaryOrbit::Graphics::RGBColor<zawa_ch::StationaryOrbit::FixedPoint32q31_t>>;
-extern template struct zawa_ch::StationaryOrbit::Graphics::TranslucentColor<zawa_ch::StationaryOrbit::Graphics::RGBColor<zawa_ch::StationaryOrbit::FixedPoint64q63_t>>;
-extern template struct zawa_ch::StationaryOrbit::Graphics::TranslucentColor<zawa_ch::StationaryOrbit::Graphics::RGBColor<float>>;
-extern template struct zawa_ch::StationaryOrbit::Graphics::TranslucentColor<zawa_ch::StationaryOrbit::Graphics::RGBColor<double>>;
+template struct
+zawa_ch::StationaryOrbit::Graphics::RGBColor<zawa_ch::StationaryOrbit::Proportion8_t>;
+template struct
+zawa_ch::StationaryOrbit::Graphics::RGBColor<zawa_ch::StationaryOrbit::Proportion16_t>;
+template struct
+zawa_ch::StationaryOrbit::Graphics::RGBColor<zawa_ch::StationaryOrbit::Proportion32_t>;
+template struct
+zawa_ch::StationaryOrbit::Graphics::RGBColor<zawa_ch::StationaryOrbit::Proportion64_t>;
+template struct
+zawa_ch::StationaryOrbit::Graphics::RGBColor<zawa_ch::StationaryOrbit::FixedPoint16q15_t>;
+template struct
+zawa_ch::StationaryOrbit::Graphics::RGBColor<zawa_ch::StationaryOrbit::FixedPoint32q31_t>;
+template struct
+zawa_ch::StationaryOrbit::Graphics::RGBColor<zawa_ch::StationaryOrbit::FixedPoint64q63_t>;
+template struct
+zawa_ch::StationaryOrbit::Graphics::RGBColor<float>;
+template struct
+zawa_ch::StationaryOrbit::Graphics::RGBColor<double>;
+template struct
+zawa_ch::StationaryOrbit::Graphics::TranslucentColor<zawa_ch::StationaryOrbit::Graphics::RGBColor<zawa_ch::StationaryOrbit::Proportion8_t>>;
+template struct
+zawa_ch::StationaryOrbit::Graphics::TranslucentColor<zawa_ch::StationaryOrbit::Graphics::RGBColor<zawa_ch::StationaryOrbit::Proportion16_t>>;
+template struct
+zawa_ch::StationaryOrbit::Graphics::TranslucentColor<zawa_ch::StationaryOrbit::Graphics::RGBColor<zawa_ch::StationaryOrbit::Proportion32_t>>;
+template struct
+zawa_ch::StationaryOrbit::Graphics::TranslucentColor<zawa_ch::StationaryOrbit::Graphics::RGBColor<zawa_ch::StationaryOrbit::Proportion64_t>>;
+template struct
+zawa_ch::StationaryOrbit::Graphics::TranslucentColor<zawa_ch::StationaryOrbit::Graphics::RGBColor<zawa_ch::StationaryOrbit::FixedPoint16q15_t>>;
+template struct
+zawa_ch::StationaryOrbit::Graphics::TranslucentColor<zawa_ch::StationaryOrbit::Graphics::RGBColor<zawa_ch::StationaryOrbit::FixedPoint32q31_t>>;
+template struct
+zawa_ch::StationaryOrbit::Graphics::TranslucentColor<zawa_ch::StationaryOrbit::Graphics::RGBColor<zawa_ch::StationaryOrbit::FixedPoint64q63_t>>;
+template struct
+zawa_ch::StationaryOrbit::Graphics::TranslucentColor<zawa_ch::StationaryOrbit::Graphics::RGBColor<float>>;
+template struct
+zawa_ch::StationaryOrbit::Graphics::TranslucentColor<zawa_ch::StationaryOrbit::Graphics::RGBColor<double>>;
