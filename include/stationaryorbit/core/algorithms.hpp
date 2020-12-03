a99d6226b90e8ae0eb8c5f248b4d48da5e9bc648
zawa-ch <satellite.2e1834097@gmail.com>
2020-12-03 21:50:32 +0900

    * Algorithm
    算術オーバーフロー検出関数追加

--- include/stationaryorbit/core/algorithms.hpp
+++ include/stationaryorbit/core/algorithms.hpp
@@ -18,6 +18,8 @@
 //
 #ifndef __stationaryorbit__core_algorithms__
 #define __stationaryorbit__core_algorithms__
+#include <stdexcept>
+#include "traits.hpp"
 namespace zawa_ch::StationaryOrbit
 {
 	///	ライブラリで使用される基本的なアルゴリズムを実装します。
@@ -70,6 +72,192 @@ namespace zawa_ch::StationaryOrbit
 				return result;
 			}
 		}
+
+		template<class Tp>
+		static constexpr bool MayOverflowWithAddition(const Tp& left, const Tp& right) noexcept
+		{
+			static_assert(Traits::IsNumeralType<Tp>, "テンプレート引数型 Tp は算術型である必要があります。");
+			static_assert(std::is_constructible_v<Tp, int>, "テンプレート引数型 Tp は (int) を引数に取るコンストラクタを持つ必要があります。");
+			if constexpr (std::true_type::value)
+			{
+				auto eval = std::numeric_limits<Tp>::max() - right;
+				if (eval < left)
+				{
+					return true;
+				}
+			}
+			if constexpr (std::numeric_limits<Tp>::is_signed)
+			{
+				if (right < Tp(0))
+				{
+					auto eval = std::numeric_limits<Tp>::lowest() - right;
+					if (left < eval)
+					{
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+		template<class Tp>
+		static constexpr bool MayOverflowWithSubtraction(const Tp& left, const Tp& right) noexcept
+		{
+			static_assert(Traits::IsNumeralType<Tp>, "テンプレート引数型 Tp は算術型である必要があります。");
+			static_assert(std::is_constructible_v<Tp, int>, "テンプレート引数型 Tp は (int) を引数に取るコンストラクタを持つ必要があります。");
+			if constexpr (std::true_type::value)
+			{
+				auto eval = std::numeric_limits<Tp>::lowest() + right;
+				if (left < eval)
+				{
+					return true;
+				}
+			}
+			if constexpr (std::numeric_limits<Tp>::is_signed)
+			{
+				if (right < Tp(0))
+				{
+					auto eval = std::numeric_limits<Tp>::max() + right;
+					if (eval < left)
+					{
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+		template<class Tp>
+		static constexpr bool MayOverflowWithMultiplication(const Tp& left, const Tp& right) noexcept
+		{
+			static_assert(Traits::IsNumeralType<Tp>, "テンプレート引数型 Tp は算術型である必要があります。");
+			static_assert(std::is_constructible_v<Tp, int>, "テンプレート引数型 Tp は (int) を引数に取るコンストラクタを持つ必要があります。");
+			if constexpr (std::numeric_limits<Tp>::is_signed)
+			{
+				if ( ( (Tp(-1) < left)&&(left <= Tp(1)) )||( (Tp(-1) < right)&&(right <= Tp(1)) ) )
+				{
+					// 一方のオペランドの絶対値が1より小さい(あるいは1に等しい)場合、演算結果はオーバーフローすることはない
+					return false;
+				}
+				if (right == Tp(-1))
+				{
+					if constexpr (std::numeric_limits<Tp>::lowest() + std::numeric_limits<Tp>::max() != Tp(0))
+					{
+						if constexpr (std::numeric_limits<Tp>::lowest() + std::numeric_limits<Tp>::max() < Tp(0))
+						{ return left < -std::numeric_limits<Tp>::max(); }
+						else
+						{ return -std::numeric_limits<Tp>::lowest() < left; }
+					}
+					else
+					{
+						return false;
+					}
+				}
+			}
+			else
+			{
+				if ( (left <= Tp(1))||(right <= Tp(1)) )
+				{
+					// 一方のオペランドが1より小さい(あるいは1に等しい)場合、演算結果はオーバーフローすることはない
+					return false;
+				}
+			}
+			auto evalc = std::numeric_limits<Tp>::max() / right;
+			auto evalf = std::numeric_limits<Tp>::lowest() / right;
+			return (left < std::min(evalc, evalf))||(std::max(evalc, evalf) < left);
+		}
+		template<class Tp>
+		static constexpr bool MayOverflowWithDivision(const Tp& left, const Tp& right) noexcept
+		{
+			static_assert(Traits::IsNumeralType<Tp>, "テンプレート引数型 Tp は算術型である必要があります。");
+			static_assert(std::is_constructible_v<Tp, int>, "テンプレート引数型 Tp は (int) を引数に取るコンストラクタを持つ必要があります。");
+			if (right == Tp(0))
+			{
+				return true;
+			}
+			if constexpr (std::numeric_limits<Tp>::is_signed)
+			{
+				if ((right < Tp(-1))||(Tp(1) <= right)) { return false; }
+			}
+			else
+			{
+				if (Tp(1) <= right) { return false; }
+			}
+			auto evalc = std::numeric_limits<Tp>::max() * right;
+			auto evalf = std::numeric_limits<Tp>::lowest() * right;
+			return (left < std::min(evalc, evalf))||(std::max(evalc, evalf) < left);
+		}
 	};
+
+	extern template bool Algorithms::MayOverflowWithAddition<bool>(const bool&, const bool&);
+	extern template bool Algorithms::MayOverflowWithAddition<char>(const char&, const char&);
+	extern template bool Algorithms::MayOverflowWithAddition<signed char>(const signed char&, const signed char&);
+	extern template bool Algorithms::MayOverflowWithAddition<unsigned char>(const unsigned char&, const unsigned char&);
+	extern template bool Algorithms::MayOverflowWithAddition<wchar_t>(const wchar_t&, const wchar_t&);
+	extern template bool Algorithms::MayOverflowWithAddition<short>(const short&, const short&);
+	extern template bool Algorithms::MayOverflowWithAddition<unsigned short>(const unsigned short&, const unsigned short&);
+	extern template bool Algorithms::MayOverflowWithAddition<int>(const int&, const int&);
+	extern template bool Algorithms::MayOverflowWithAddition<unsigned int>(const unsigned int&, const unsigned int&);
+	extern template bool Algorithms::MayOverflowWithAddition<long>(const long&, const long&);
+	extern template bool Algorithms::MayOverflowWithAddition<unsigned long>(const unsigned long&, const unsigned long&);
+	extern template bool Algorithms::MayOverflowWithAddition<long long>(const long long&, const long long&);
+	extern template bool Algorithms::MayOverflowWithAddition<unsigned long long>(const unsigned long long&, const unsigned long long&);
+	extern template bool Algorithms::MayOverflowWithAddition<float>(const float&, const float&);
+	extern template bool Algorithms::MayOverflowWithAddition<double>(const double&, const double&);
+	extern template bool Algorithms::MayOverflowWithAddition<long double>(const long double&, const long double&);
+	extern template bool Algorithms::MayOverflowWithAddition<char16_t>(const char16_t&, const char16_t&);
+	extern template bool Algorithms::MayOverflowWithAddition<char32_t>(const char32_t&, const char32_t&);
+	extern template bool Algorithms::MayOverflowWithSubtraction<bool>(const bool&, const bool&);
+	extern template bool Algorithms::MayOverflowWithSubtraction<char>(const char&, const char&);
+	extern template bool Algorithms::MayOverflowWithSubtraction<signed char>(const signed char&, const signed char&);
+	extern template bool Algorithms::MayOverflowWithSubtraction<unsigned char>(const unsigned char&, const unsigned char&);
+	extern template bool Algorithms::MayOverflowWithSubtraction<wchar_t>(const wchar_t&, const wchar_t&);
+	extern template bool Algorithms::MayOverflowWithSubtraction<short>(const short&, const short&);
+	extern template bool Algorithms::MayOverflowWithSubtraction<unsigned short>(const unsigned short&, const unsigned short&);
+	extern template bool Algorithms::MayOverflowWithSubtraction<int>(const int&, const int&);
+	extern template bool Algorithms::MayOverflowWithSubtraction<unsigned int>(const unsigned int&, const unsigned int&);
+	extern template bool Algorithms::MayOverflowWithSubtraction<long>(const long&, const long&);
+	extern template bool Algorithms::MayOverflowWithSubtraction<unsigned long>(const unsigned long&, const unsigned long&);
+	extern template bool Algorithms::MayOverflowWithSubtraction<long long>(const long long&, const long long&);
+	extern template bool Algorithms::MayOverflowWithSubtraction<unsigned long long>(const unsigned long long&, const unsigned long long&);
+	extern template bool Algorithms::MayOverflowWithSubtraction<float>(const float&, const float&);
+	extern template bool Algorithms::MayOverflowWithSubtraction<double>(const double&, const double&);
+	extern template bool Algorithms::MayOverflowWithSubtraction<long double>(const long double&, const long double&);
+	extern template bool Algorithms::MayOverflowWithSubtraction<char16_t>(const char16_t&, const char16_t&);
+	extern template bool Algorithms::MayOverflowWithSubtraction<char32_t>(const char32_t&, const char32_t&);
+	extern template bool Algorithms::MayOverflowWithMultiplication<bool>(const bool&, const bool&);
+	extern template bool Algorithms::MayOverflowWithMultiplication<char>(const char&, const char&);
+	extern template bool Algorithms::MayOverflowWithMultiplication<signed char>(const signed char&, const signed char&);
+	extern template bool Algorithms::MayOverflowWithMultiplication<unsigned char>(const unsigned char&, const unsigned char&);
+	extern template bool Algorithms::MayOverflowWithMultiplication<wchar_t>(const wchar_t&, const wchar_t&);
+	extern template bool Algorithms::MayOverflowWithMultiplication<short>(const short&, const short&);
+	extern template bool Algorithms::MayOverflowWithMultiplication<unsigned short>(const unsigned short&, const unsigned short&);
+	extern template bool Algorithms::MayOverflowWithMultiplication<int>(const int&, const int&);
+	extern template bool Algorithms::MayOverflowWithMultiplication<unsigned int>(const unsigned int&, const unsigned int&);
+	extern template bool Algorithms::MayOverflowWithMultiplication<long>(const long&, const long&);
+	extern template bool Algorithms::MayOverflowWithMultiplication<unsigned long>(const unsigned long&, const unsigned long&);
+	extern template bool Algorithms::MayOverflowWithMultiplication<long long>(const long long&, const long long&);
+	extern template bool Algorithms::MayOverflowWithMultiplication<unsigned long long>(const unsigned long long&, const unsigned long long&);
+	extern template bool Algorithms::MayOverflowWithMultiplication<float>(const float&, const float&);
+	extern template bool Algorithms::MayOverflowWithMultiplication<double>(const double&, const double&);
+	extern template bool Algorithms::MayOverflowWithMultiplication<long double>(const long double&, const long double&);
+	extern template bool Algorithms::MayOverflowWithMultiplication<char16_t>(const char16_t&, const char16_t&);
+	extern template bool Algorithms::MayOverflowWithMultiplication<char32_t>(const char32_t&, const char32_t&);
+	extern template bool Algorithms::MayOverflowWithDivision<bool>(const bool&, const bool&);
+	extern template bool Algorithms::MayOverflowWithDivision<char>(const char&, const char&);
+	extern template bool Algorithms::MayOverflowWithDivision<signed char>(const signed char&, const signed char&);
+	extern template bool Algorithms::MayOverflowWithDivision<unsigned char>(const unsigned char&, const unsigned char&);
+	extern template bool Algorithms::MayOverflowWithDivision<wchar_t>(const wchar_t&, const wchar_t&);
+	extern template bool Algorithms::MayOverflowWithDivision<short>(const short&, const short&);
+	extern template bool Algorithms::MayOverflowWithDivision<unsigned short>(const unsigned short&, const unsigned short&);
+	extern template bool Algorithms::MayOverflowWithDivision<int>(const int&, const int&);
+	extern template bool Algorithms::MayOverflowWithDivision<unsigned int>(const unsigned int&, const unsigned int&);
+	extern template bool Algorithms::MayOverflowWithDivision<long>(const long&, const long&);
+	extern template bool Algorithms::MayOverflowWithDivision<unsigned long>(const unsigned long&, const unsigned long&);
+	extern template bool Algorithms::MayOverflowWithDivision<long long>(const long long&, const long long&);
+	extern template bool Algorithms::MayOverflowWithDivision<unsigned long long>(const unsigned long long&, const unsigned long long&);
+	extern template bool Algorithms::MayOverflowWithDivision<float>(const float&, const float&);
+	extern template bool Algorithms::MayOverflowWithDivision<double>(const double&, const double&);
+	extern template bool Algorithms::MayOverflowWithDivision<long double>(const long double&, const long double&);
+	extern template bool Algorithms::MayOverflowWithDivision<char16_t>(const char16_t&, const char16_t&);
+	extern template bool Algorithms::MayOverflowWithDivision<char32_t>(const char32_t&, const char32_t&);
 }
 #endif // __stationaryorbit__core_algorithms__
---
+++ src/core/algorithms.cpp
@@ -0,0 +1,93 @@
+//	stationaryorbit.core:/algorithms
+//	Copyright 2020 zawa-ch.
+//	GPLv3 (or later) license
+//
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	any later version.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//	See the GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with this program.
+//	If not, see <http://www.gnu.org/licenses/>.
+//
+#include "stationaryorbit/core/algorithms.hpp"
+
+
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithAddition<bool>(const bool&, const bool&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithAddition<char>(const char&, const char&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithAddition<signed char>(const signed char&, const signed char&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithAddition<unsigned char>(const unsigned char&, const unsigned char&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithAddition<wchar_t>(const wchar_t&, const wchar_t&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithAddition<short>(const short&, const short&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithAddition<unsigned short>(const unsigned short&, const unsigned short&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithAddition<int>(const int&, const int&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithAddition<unsigned int>(const unsigned int&, const unsigned int&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithAddition<long>(const long&, const long&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithAddition<unsigned long>(const unsigned long&, const unsigned long&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithAddition<long long>(const long long&, const long long&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithAddition<unsigned long long>(const unsigned long long&, const unsigned long long&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithAddition<float>(const float&, const float&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithAddition<double>(const double&, const double&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithAddition<long double>(const long double&, const long double&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithAddition<char16_t>(const char16_t&, const char16_t&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithAddition<char32_t>(const char32_t&, const char32_t&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithSubtraction<bool>(const bool&, const bool&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithSubtraction<char>(const char&, const char&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithSubtraction<signed char>(const signed char&, const signed char&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithSubtraction<unsigned char>(const unsigned char&, const unsigned char&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithSubtraction<wchar_t>(const wchar_t&, const wchar_t&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithSubtraction<short>(const short&, const short&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithSubtraction<unsigned short>(const unsigned short&, const unsigned short&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithSubtraction<int>(const int&, const int&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithSubtraction<unsigned int>(const unsigned int&, const unsigned int&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithSubtraction<long>(const long&, const long&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithSubtraction<unsigned long>(const unsigned long&, const unsigned long&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithSubtraction<long long>(const long long&, const long long&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithSubtraction<unsigned long long>(const unsigned long long&, const unsigned long long&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithSubtraction<float>(const float&, const float&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithSubtraction<double>(const double&, const double&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithSubtraction<long double>(const long double&, const long double&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithSubtraction<char16_t>(const char16_t&, const char16_t&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithSubtraction<char32_t>(const char32_t&, const char32_t&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithMultiplication<bool>(const bool&, const bool&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithMultiplication<char>(const char&, const char&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithMultiplication<signed char>(const signed char&, const signed char&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithMultiplication<unsigned char>(const unsigned char&, const unsigned char&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithMultiplication<wchar_t>(const wchar_t&, const wchar_t&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithMultiplication<short>(const short&, const short&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithMultiplication<unsigned short>(const unsigned short&, const unsigned short&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithMultiplication<int>(const int&, const int&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithMultiplication<unsigned int>(const unsigned int&, const unsigned int&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithMultiplication<long>(const long&, const long&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithMultiplication<unsigned long>(const unsigned long&, const unsigned long&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithMultiplication<long long>(const long long&, const long long&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithMultiplication<unsigned long long>(const unsigned long long&, const unsigned long long&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithMultiplication<float>(const float&, const float&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithMultiplication<double>(const double&, const double&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithMultiplication<long double>(const long double&, const long double&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithMultiplication<char16_t>(const char16_t&, const char16_t&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithMultiplication<char32_t>(const char32_t&, const char32_t&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithDivision<bool>(const bool&, const bool&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithDivision<char>(const char&, const char&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithDivision<signed char>(const signed char&, const signed char&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithDivision<unsigned char>(const unsigned char&, const unsigned char&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithDivision<wchar_t>(const wchar_t&, const wchar_t&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithDivision<short>(const short&, const short&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithDivision<unsigned short>(const unsigned short&, const unsigned short&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithDivision<int>(const int&, const int&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithDivision<unsigned int>(const unsigned int&, const unsigned int&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithDivision<long>(const long&, const long&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithDivision<unsigned long>(const unsigned long&, const unsigned long&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithDivision<long long>(const long long&, const long long&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithDivision<unsigned long long>(const unsigned long long&, const unsigned long long&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithDivision<float>(const float&, const float&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithDivision<double>(const double&, const double&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithDivision<long double>(const long double&, const long double&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithDivision<char16_t>(const char16_t&, const char16_t&);
+template bool zawa_ch::StationaryOrbit::Algorithms::MayOverflowWithDivision<char32_t>(const char32_t&, const char32_t&);
