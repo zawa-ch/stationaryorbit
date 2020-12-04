554ea4562c0fe4393cfa9432583d4567c1244d5f
zawa-ch <satellite.2e1834097@gmail.com>
2020-12-04 16:55:07 +0900

    * MultipleULong
    除算演算子をDivide()を間接的に呼び出すよう変更

--- include/stationaryorbit/core/multiplelong.hpp
+++ include/stationaryorbit/core/multiplelong.hpp
@@ -105,43 +105,11 @@ namespace zawa_ch::StationaryOrbit
 		}
 		[[nodiscard]] constexpr MultipleULong<T, N> operator/(const MultipleULong<T, N>& other) const noexcept
 		{
-			size_t w = 0;
-			for (auto i: Range<size_t>(0, BitWidth<T> * N).GetStdIterator())
-			{
-				if (((other << i) & (MultipleULong<T, N>(1U) << (BitWidth<T> * N - 1))) != 0) { w = i; break; }
-			}
-			auto result = MultipleULong<T, N>();
-			MultipleULong<T, N> surplus = *this;
-			for (auto i: Range<size_t>(0, w).GetStdReverseIterator())
-			{
-				MultipleULong<T, N> div = other << i;
-				if (div <= surplus)
-				{
-					result |= MultipleULong<T, N>(1) << i;
-					surplus -= div;
-				}
-			}
-			return result;
+			return Divide(other).Value;
 		}
 		[[nodiscard]] constexpr MultipleULong<T, N> operator%(const MultipleULong<T, N>& other) const noexcept
 		{
-			size_t w = 0;
-			for (auto i: Range<size_t>(0, BitWidth<T> * N).GetStdIterator())
-			{
-				if (((other << i) & (MultipleULong<T, N>(1U) << (BitWidth<T> * N - 1))) != 0) { w = i; }
-			}
-			auto result = MultipleULong<T, N>();
-			MultipleULong<T, N> surplus = *this;
-			for (auto i: Range<size_t>(0, w).GetStdReverseIterator())
-			{
-				MultipleULong<T, N> div = other << i;
-				if (div <= surplus)
-				{
-					result |= MultipleULong<T, N>(1) << i;
-					surplus -= div;
-				}
-			}
-			return surplus;
+			return Divide(other).Mod;
 		}
 		[[nodiscard]] constexpr MultipleULong<T, N> operator~() const noexcept
 		{
