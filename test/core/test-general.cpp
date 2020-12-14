d84969d0551f88e5d240b1de81872b8cda5483b9
zawa-ch <satellite.2e1834097@gmail.com>
2020-12-14 19:20:22 +0900

    * core.test Traitsテストコードの呼び出しを削除

--- test/core/test-general.cpp
+++ test/core/test-general.cpp
@@ -21,11 +21,9 @@ void Test_BitOperation();
 void Test_Iteration();
 void Test_Delegate();
 int Test_Property();
-void Test_Traits();
 
 int main(int argc, char const *argv[])
 {
-	Test_Traits();
 	Test_Numeral();
 	Test_BitOperation();
 	Test_Iteration();
