9920c8eaa56fd5214f58e935d27faf0e9f472c3b
zawa-ch <satellite.2e1834097@gmail.com>
2020-12-16 17:56:23 +0900

    - RectangleSize

--- include/stationaryorbit/core/rectanglesize.hpp
+++ include/stationaryorbit/core/rectanglesize.hpp
@@ -24,9 +24,6 @@
 #include "range.hpp"
 namespace zawa_ch::StationaryOrbit
 {
-	struct RectangleSize;
-	struct RectangleSizeF;
-
 	///	幅・高さを持つオブジェクトの大きさを表します。
 	template<class T, class = void>
 	struct Rect2DSize final
@@ -148,99 +145,5 @@ namespace zawa_ch::StationaryOrbit
 	extern template struct Rect2DSize<int64_t>;
 	extern template struct Rect2DSize<float>;
 	extern template struct Rect2DSize<double>;
-
-	///	幅・高さを持つオブジェクトの大きさを表します。
-	struct RectangleSize final
-	{
-	private: // contains
-		int _w;
-		int _h;
-	public: // construct
-		constexpr RectangleSize() : _w(), _h() {}
-		constexpr RectangleSize(const int& width, const int& height) : _w(width), _h(height) {}
-	public: // copy/move/destruct
-		RectangleSize(const RectangleSize&) = default;
-		RectangleSize& operator=(const RectangleSize&) = default;
-		RectangleSize(RectangleSize&&) = default;
-		RectangleSize& operator=(RectangleSize&&) = default;
-		~RectangleSize() = default;
-	public: // member
-		///	水平軸の値を取得します。
-		constexpr const int& Width() const { return _w; }
-		///	垂直軸の値を取得します。
-		constexpr const int& Height() const { return _h; }
-		///	水平軸コンポーネントの値の範囲を示す @a Range を取得します。
-		constexpr Range<int> XRange() const { return Range(0, _w); }
-		///	垂直軸コンポーネントの値の範囲を示す @a Range を取得します。
-		constexpr Range<int> YRange() const { return Range(0, _h); }
-		constexpr bool IsEmpty() const { return (_w==0)&&(_h==0); }
-
-		constexpr RectangleSize operator+(const RectangleSize& other) const { return RectangleSize(_w + other._w, _h + other._h); }
-		constexpr RectangleSize& operator+=(const RectangleSize& other) { return *this = *this + other; }
-		constexpr RectangleSize operator-(const RectangleSize& other) const { return RectangleSize(_w - other._w, _h - other._h); }
-		constexpr RectangleSize& operator-=(const RectangleSize& other) { return *this = *this - other; }
-
-		///	指定されたオブジェクトがこのオブジェクトと等価であることをテストします。
-		constexpr bool Equals(const RectangleSize& value) const { return (_w == value._w)&&(_h == value._h); }
-		constexpr bool operator==(const RectangleSize& value) const { return Equals(value); }
-		constexpr bool operator!=(const RectangleSize& value) const { return !Equals(value); }
-
-		///	空の @a RectangleSize を表します。
-		static constexpr RectangleSize Empty() { return RectangleSize{}; }
-	};
-	///	幅・高さを持つオブジェクトの大きさを表します。
-	struct RectangleSizeF final
-	{
-	private: // contains
-		float _w;
-		float _h;
-	public: // construct
-		constexpr RectangleSizeF() : _w(), _h() {}
-		constexpr RectangleSizeF(const float& width, const float& height) : _w(width), _h(height) {}
-		constexpr RectangleSizeF(const RectangleSize& value) : _w(value.Width()), _h(value.Height()) {}
-	public: // copy/move/destruct
-		RectangleSizeF(const RectangleSizeF&) = default;
-		RectangleSizeF& operator=(const RectangleSizeF&) = default;
-		RectangleSizeF(RectangleSizeF&&) = default;
-		RectangleSizeF& operator=(RectangleSizeF&&) = default;
-		~RectangleSizeF() = default;
-	public: // member
-		///	水平軸コンポーネントを取得します。
-		constexpr const float& Width() const { return _w; }
-		///	垂直軸コンポーネントを取得します。
-		constexpr const float& Height() const { return _h; }
-		constexpr bool IsEmpty() const { return (_w==0)&&(_h==0); }
-
-		constexpr RectangleSizeF operator+(const RectangleSizeF& other) const { return RectangleSizeF(_w + other._w, _h + other._h); }
-		constexpr RectangleSizeF& operator+=(const RectangleSizeF& other) { return *this = *this + other; }
-		constexpr RectangleSizeF operator-(const RectangleSizeF& other) const { return RectangleSizeF(_w - other._w, _h - other._h); }
-		constexpr RectangleSizeF& operator-=(const RectangleSizeF& other) { return *this = *this - other; }
-		constexpr RectangleSizeF operator*(const RectangleSizeF& other) const { return RectangleSizeF(_w * other._w, _h * other._h); }
-		constexpr RectangleSizeF operator*(const float& other) const { return RectangleSizeF(_w * other, _h * other); }
-		constexpr RectangleSizeF& operator*=(const float& other) { return *this = *this * other; }
-		constexpr RectangleSizeF operator/(const RectangleSizeF& other) const { return RectangleSizeF(_w / other._w, _h / other._h); }
-		constexpr RectangleSizeF operator/(const float& other) const { return RectangleSizeF(_w / other, _h / other); }
-		constexpr RectangleSizeF& operator/=(const float& other) { return *this = *this / other; }
-
-		///	指定されたオブジェクトがこのオブジェクトと等価であることをテストします。
-		constexpr bool Equals(const RectangleSizeF& other) const { return (_w == other._w)&&(_h == other._h); }
-		constexpr bool operator==(const RectangleSizeF& other) const { return Equals(other); }
-		constexpr bool operator!=(const RectangleSizeF& other) const { return !Equals(other); }
-
-		constexpr explicit operator RectangleSize() const { return RectangleSize(int(_w), int(_h)); }
-
-		///	@a RectangleSizeF の小数部を抜き出します。
-		RectangleSizeF Extract() const { return RectangleSizeF(_w - truncf(_w), _h - truncf(_h)); }
-		///	@a RectangleSizeF を切り捨て方向に丸め、 @a RectangleSize に変換します。
-		RectangleSize Floor() const { return RectangleSize(floorf(_w), floorf(_h)); }
-		///	@a RectangleSizeF を切り上げ方向に丸め、 @a RectangleSize に変換します。
-		RectangleSize Ceiling() const { return RectangleSize(ceilf(_w), ceilf(_h)); }
-		/// @a RectangleSizeF を最も近い @a RectangleSize に変換します。
-		RectangleSize Round() const { return RectangleSize(roundf(_w), roundf(_h)); }
-
-		///	空の @a RectangleSizeF を表します。
-		static constexpr RectangleSizeF Empty() { return RectangleSizeF{}; }
-	};
-
 }
 #endif // __stationaryorbit_core_size__
