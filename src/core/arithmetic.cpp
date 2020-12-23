8d53d8f9125761dbf8ccb19db646a04f0ee0b8f9
zawa-ch <satellite.2e1834097@gmail.com>
2020-12-23 16:10:17 +0900

    * ArithmeticOperation テンプレート実体化
    Add/Subtract/Multiply/Divideの基本算術型に対する実体化

---
+++ src/core/arithmetic.cpp
@@ -0,0 +1,93 @@
+//	stationaryorbit.core:/arithmetic
+//	Copyright 2020 zawa-ch.
+//	GPLv3 (or later) license
+//
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	any later version.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//	See the GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with this program.
+//	If not, see <http://www.gnu.org/licenses/>.
+//
+#include "stationaryorbit/core/arithmetic.hpp"
+using namespace zawa_ch::StationaryOrbit;
+
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<bool> zawa_ch::StationaryOrbit::ArithmeticOperation::Add<bool>(const bool&, const bool&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<char> zawa_ch::StationaryOrbit::ArithmeticOperation::Add<char>(const char&, const char&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<signed char> zawa_ch::StationaryOrbit::ArithmeticOperation::Add<signed char>(const signed char&, const signed char&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<unsigned char> zawa_ch::StationaryOrbit::ArithmeticOperation::Add<unsigned char>(const unsigned char&, const unsigned char&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<wchar_t> zawa_ch::StationaryOrbit::ArithmeticOperation::Add<wchar_t>(const wchar_t&, const wchar_t&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<short> zawa_ch::StationaryOrbit::ArithmeticOperation::Add<short>(const short&, const short&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<unsigned short> zawa_ch::StationaryOrbit::ArithmeticOperation::Add<unsigned short>(const unsigned short&, const unsigned short&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<int> zawa_ch::StationaryOrbit::ArithmeticOperation::Add<int>(const int&, const int&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<unsigned int> zawa_ch::StationaryOrbit::ArithmeticOperation::Add<unsigned int>(const unsigned int&, const unsigned int&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<long> zawa_ch::StationaryOrbit::ArithmeticOperation::Add<long>(const long&, const long&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<unsigned long> zawa_ch::StationaryOrbit::ArithmeticOperation::Add<unsigned long>(const unsigned long&, const unsigned long&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<long long> zawa_ch::StationaryOrbit::ArithmeticOperation::Add<long long>(const long long&, const long long&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<unsigned long long> zawa_ch::StationaryOrbit::ArithmeticOperation::Add<unsigned long long>(const unsigned long long&, const unsigned long long&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<float> zawa_ch::StationaryOrbit::ArithmeticOperation::Add<float>(const float&, const float&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<double> zawa_ch::StationaryOrbit::ArithmeticOperation::Add<double>(const double&, const double&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<long double> zawa_ch::StationaryOrbit::ArithmeticOperation::Add<long double>(const long double&, const long double&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<char16_t> zawa_ch::StationaryOrbit::ArithmeticOperation::Add<char16_t>(const char16_t&, const char16_t&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<char32_t> zawa_ch::StationaryOrbit::ArithmeticOperation::Add<char32_t>(const char32_t&, const char32_t&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<bool> zawa_ch::StationaryOrbit::ArithmeticOperation::Subtract<bool>(const bool&, const bool&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<char> zawa_ch::StationaryOrbit::ArithmeticOperation::Subtract<char>(const char&, const char&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<signed char> zawa_ch::StationaryOrbit::ArithmeticOperation::Subtract<signed char>(const signed char&, const signed char&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<unsigned char> zawa_ch::StationaryOrbit::ArithmeticOperation::Subtract<unsigned char>(const unsigned char&, const unsigned char&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<wchar_t> zawa_ch::StationaryOrbit::ArithmeticOperation::Subtract<wchar_t>(const wchar_t&, const wchar_t&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<short> zawa_ch::StationaryOrbit::ArithmeticOperation::Subtract<short>(const short&, const short&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<unsigned short> zawa_ch::StationaryOrbit::ArithmeticOperation::Subtract<unsigned short>(const unsigned short&, const unsigned short&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<int> zawa_ch::StationaryOrbit::ArithmeticOperation::Subtract<int>(const int&, const int&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<unsigned int> zawa_ch::StationaryOrbit::ArithmeticOperation::Subtract<unsigned int>(const unsigned int&, const unsigned int&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<long> zawa_ch::StationaryOrbit::ArithmeticOperation::Subtract<long>(const long&, const long&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<unsigned long> zawa_ch::StationaryOrbit::ArithmeticOperation::Subtract<unsigned long>(const unsigned long&, const unsigned long&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<long long> zawa_ch::StationaryOrbit::ArithmeticOperation::Subtract<long long>(const long long&, const long long&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<unsigned long long> zawa_ch::StationaryOrbit::ArithmeticOperation::Subtract<unsigned long long>(const unsigned long long&, const unsigned long long&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<float> zawa_ch::StationaryOrbit::ArithmeticOperation::Subtract<float>(const float&, const float&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<double> zawa_ch::StationaryOrbit::ArithmeticOperation::Subtract<double>(const double&, const double&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<long double> zawa_ch::StationaryOrbit::ArithmeticOperation::Subtract<long double>(const long double&, const long double&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<char16_t> zawa_ch::StationaryOrbit::ArithmeticOperation::Subtract<char16_t>(const char16_t&, const char16_t&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::AdditionResult<char32_t> zawa_ch::StationaryOrbit::ArithmeticOperation::Subtract<char32_t>(const char32_t&, const char32_t&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<bool> zawa_ch::StationaryOrbit::ArithmeticOperation::Multiply<bool>(const bool&, const bool&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<char> zawa_ch::StationaryOrbit::ArithmeticOperation::Multiply<char>(const char&, const char&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<signed char> zawa_ch::StationaryOrbit::ArithmeticOperation::Multiply<signed char>(const signed char&, const signed char&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<unsigned char> zawa_ch::StationaryOrbit::ArithmeticOperation::Multiply<unsigned char>(const unsigned char&, const unsigned char&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<wchar_t> zawa_ch::StationaryOrbit::ArithmeticOperation::Multiply<wchar_t>(const wchar_t&, const wchar_t&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<short> zawa_ch::StationaryOrbit::ArithmeticOperation::Multiply<short>(const short&, const short&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<unsigned short> zawa_ch::StationaryOrbit::ArithmeticOperation::Multiply<unsigned short>(const unsigned short&, const unsigned short&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<int> zawa_ch::StationaryOrbit::ArithmeticOperation::Multiply<int>(const int&, const int&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<unsigned int> zawa_ch::StationaryOrbit::ArithmeticOperation::Multiply<unsigned int>(const unsigned int&, const unsigned int&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<long> zawa_ch::StationaryOrbit::ArithmeticOperation::Multiply<long>(const long&, const long&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<unsigned long> zawa_ch::StationaryOrbit::ArithmeticOperation::Multiply<unsigned long>(const unsigned long&, const unsigned long&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<long long> zawa_ch::StationaryOrbit::ArithmeticOperation::Multiply<long long>(const long long&, const long long&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<unsigned long long> zawa_ch::StationaryOrbit::ArithmeticOperation::Multiply<unsigned long long>(const unsigned long long&, const unsigned long long&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<float> zawa_ch::StationaryOrbit::ArithmeticOperation::Multiply<float>(const float&, const float&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<double> zawa_ch::StationaryOrbit::ArithmeticOperation::Multiply<double>(const double&, const double&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<long double> zawa_ch::StationaryOrbit::ArithmeticOperation::Multiply<long double>(const long double&, const long double&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<char16_t> zawa_ch::StationaryOrbit::ArithmeticOperation::Multiply<char16_t>(const char16_t&, const char16_t&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<char32_t> zawa_ch::StationaryOrbit::ArithmeticOperation::Multiply<char32_t>(const char32_t&, const char32_t&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<bool> zawa_ch::StationaryOrbit::ArithmeticOperation::Divide<bool>(const bool&, const bool&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<char> zawa_ch::StationaryOrbit::ArithmeticOperation::Divide<char>(const char&, const char&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<signed char> zawa_ch::StationaryOrbit::ArithmeticOperation::Divide<signed char>(const signed char&, const signed char&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<unsigned char> zawa_ch::StationaryOrbit::ArithmeticOperation::Divide<unsigned char>(const unsigned char&, const unsigned char&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<wchar_t> zawa_ch::StationaryOrbit::ArithmeticOperation::Divide<wchar_t>(const wchar_t&, const wchar_t&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<short> zawa_ch::StationaryOrbit::ArithmeticOperation::Divide<short>(const short&, const short&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<unsigned short> zawa_ch::StationaryOrbit::ArithmeticOperation::Divide<unsigned short>(const unsigned short&, const unsigned short&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<int> zawa_ch::StationaryOrbit::ArithmeticOperation::Divide<int>(const int&, const int&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<unsigned int> zawa_ch::StationaryOrbit::ArithmeticOperation::Divide<unsigned int>(const unsigned int&, const unsigned int&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<long> zawa_ch::StationaryOrbit::ArithmeticOperation::Divide<long>(const long&, const long&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<unsigned long> zawa_ch::StationaryOrbit::ArithmeticOperation::Divide<unsigned long>(const unsigned long&, const unsigned long&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<long long> zawa_ch::StationaryOrbit::ArithmeticOperation::Divide<long long>(const long long&, const long long&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<unsigned long long> zawa_ch::StationaryOrbit::ArithmeticOperation::Divide<unsigned long long>(const unsigned long long&, const unsigned long long&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<float> zawa_ch::StationaryOrbit::ArithmeticOperation::Divide<float>(const float&, const float&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<double> zawa_ch::StationaryOrbit::ArithmeticOperation::Divide<double>(const double&, const double&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<long double> zawa_ch::StationaryOrbit::ArithmeticOperation::Divide<long double>(const long double&, const long double&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<char16_t> zawa_ch::StationaryOrbit::ArithmeticOperation::Divide<char16_t>(const char16_t&, const char16_t&);
+template zawa_ch::StationaryOrbit::ArithmeticOperation::MultiplicationResult<char32_t> zawa_ch::StationaryOrbit::ArithmeticOperation::Divide<char32_t>(const char32_t&, const char32_t&);
