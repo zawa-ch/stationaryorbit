daf924b51ca1aa30630622106150d7e7a3369e7f
zawa-ch <satellite.2e1834097@gmail.com>
2020-12-23 17:52:52 +0900

    * ArithmeticOperation MayOverflowWithをAlgorithmsから移動

--- include/stationaryorbit/core/arithmetic.hpp
+++ include/stationaryorbit/core/arithmetic.hpp
@@ -261,6 +261,118 @@ namespace zawa_ch::StationaryOrbit
 				default: { throw InvalidOperationException("計算結果の状態が定義されていない状態になりました。"); }
 			}
 		}
+		template<class Tp>
+		static constexpr bool MayOverflowWithAddition(const Tp& left, const Tp& right) noexcept
+		{
+			static_assert(Traits::IsNumeralType<Tp>, "テンプレート引数型 Tp は算術型である必要があります。");
+			static_assert(std::is_constructible_v<Tp, int>, "テンプレート引数型 Tp は (int) を引数に取るコンストラクタを持つ必要があります。");
+			if constexpr (std::true_type::value)
+			{
+				auto eval = std::numeric_limits<Tp>::max() - right;
+				if (eval < left)
+				{
+					return true;
+				}
+			}
+			if constexpr (std::numeric_limits<Tp>::is_signed)
+			{
+				if (right < Tp(0))
+				{
+					auto eval = std::numeric_limits<Tp>::lowest() - right;
+					if (left < eval)
+					{
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+		template<class Tp>
+		static constexpr bool MayOverflowWithSubtraction(const Tp& left, const Tp& right) noexcept
+		{
+			static_assert(Traits::IsNumeralType<Tp>, "テンプレート引数型 Tp は算術型である必要があります。");
+			static_assert(std::is_constructible_v<Tp, int>, "テンプレート引数型 Tp は (int) を引数に取るコンストラクタを持つ必要があります。");
+			if constexpr (std::true_type::value)
+			{
+				auto eval = std::numeric_limits<Tp>::lowest() + right;
+				if (left < eval)
+				{
+					return true;
+				}
+			}
+			if constexpr (std::numeric_limits<Tp>::is_signed)
+			{
+				if (right < Tp(0))
+				{
+					auto eval = std::numeric_limits<Tp>::max() + right;
+					if (eval < left)
+					{
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+		template<class Tp>
+		static constexpr bool MayOverflowWithMultiplication(const Tp& left, const Tp& right) noexcept
+		{
+			static_assert(Traits::IsNumeralType<Tp>, "テンプレート引数型 Tp は算術型である必要があります。");
+			static_assert(std::is_constructible_v<Tp, int>, "テンプレート引数型 Tp は (int) を引数に取るコンストラクタを持つ必要があります。");
+			if constexpr (std::numeric_limits<Tp>::is_signed)
+			{
+				if ( ( (Tp(-1) < left)&&(left <= Tp(1)) )||( (Tp(-1) < right)&&(right <= Tp(1)) ) )
+				{
+					// 一方のオペランドの絶対値が1より小さい(あるいは1に等しい)場合、演算結果はオーバーフローすることはない
+					return false;
+				}
+				if (right == Tp(-1))
+				{
+					if constexpr (std::numeric_limits<Tp>::lowest() + std::numeric_limits<Tp>::max() != Tp(0))
+					{
+						if constexpr (std::numeric_limits<Tp>::lowest() + std::numeric_limits<Tp>::max() < Tp(0))
+						{ return left < -std::numeric_limits<Tp>::max(); }
+						else
+						{ return -std::numeric_limits<Tp>::lowest() < left; }
+					}
+					else
+					{
+						return false;
+					}
+				}
+			}
+			else
+			{
+				if ( (left <= Tp(1))||(right <= Tp(1)) )
+				{
+					// 一方のオペランドが1より小さい(あるいは1に等しい)場合、演算結果はオーバーフローすることはない
+					return false;
+				}
+			}
+			auto evalc = std::numeric_limits<Tp>::max() / right;
+			auto evalf = std::numeric_limits<Tp>::lowest() / right;
+			return (left < std::min(evalc, evalf))||(std::max(evalc, evalf) < left);
+		}
+		template<class Tp>
+		static constexpr bool MayOverflowWithDivision(const Tp& left, const Tp& right) noexcept
+		{
+			static_assert(Traits::IsNumeralType<Tp>, "テンプレート引数型 Tp は算術型である必要があります。");
+			static_assert(std::is_constructible_v<Tp, int>, "テンプレート引数型 Tp は (int) を引数に取るコンストラクタを持つ必要があります。");
+			if (right == Tp(0))
+			{
+				return true;
+			}
+			if constexpr (std::numeric_limits<Tp>::is_signed)
+			{
+				if ((right < Tp(-1))||(Tp(1) <= right)) { return false; }
+			}
+			else
+			{
+				if (Tp(1) <= right) { return false; }
+			}
+			auto evalc = std::numeric_limits<Tp>::max() * right;
+			auto evalf = std::numeric_limits<Tp>::lowest() * right;
+			return (left < std::min(evalc, evalf))||(std::max(evalc, evalf) < left);
+		}
 	};
 
 	extern template ArithmeticOperation::AdditionResult<bool> ArithmeticOperation::Add<bool>(const bool&, const bool&);
@@ -479,5 +591,77 @@ namespace zawa_ch::StationaryOrbit
 	extern template long double ArithmeticOperation::CheckedDivide<long double>(const long double&, const long double&);
 	extern template char16_t ArithmeticOperation::CheckedDivide<char16_t>(const char16_t&, const char16_t&);
 	extern template char32_t ArithmeticOperation::CheckedDivide<char32_t>(const char32_t&, const char32_t&);
+	extern template bool ArithmeticOperation::MayOverflowWithAddition<bool>(const bool&, const bool&);
+	extern template bool ArithmeticOperation::MayOverflowWithAddition<char>(const char&, const char&);
+	extern template bool ArithmeticOperation::MayOverflowWithAddition<signed char>(const signed char&, const signed char&);
+	extern template bool ArithmeticOperation::MayOverflowWithAddition<unsigned char>(const unsigned char&, const unsigned char&);
+	extern template bool ArithmeticOperation::MayOverflowWithAddition<wchar_t>(const wchar_t&, const wchar_t&);
+	extern template bool ArithmeticOperation::MayOverflowWithAddition<short>(const short&, const short&);
+	extern template bool ArithmeticOperation::MayOverflowWithAddition<unsigned short>(const unsigned short&, const unsigned short&);
+	extern template bool ArithmeticOperation::MayOverflowWithAddition<int>(const int&, const int&);
+	extern template bool ArithmeticOperation::MayOverflowWithAddition<unsigned int>(const unsigned int&, const unsigned int&);
+	extern template bool ArithmeticOperation::MayOverflowWithAddition<long>(const long&, const long&);
+	extern template bool ArithmeticOperation::MayOverflowWithAddition<unsigned long>(const unsigned long&, const unsigned long&);
+	extern template bool ArithmeticOperation::MayOverflowWithAddition<long long>(const long long&, const long long&);
+	extern template bool ArithmeticOperation::MayOverflowWithAddition<unsigned long long>(const unsigned long long&, const unsigned long long&);
+	extern template bool ArithmeticOperation::MayOverflowWithAddition<float>(const float&, const float&);
+	extern template bool ArithmeticOperation::MayOverflowWithAddition<double>(const double&, const double&);
+	extern template bool ArithmeticOperation::MayOverflowWithAddition<long double>(const long double&, const long double&);
+	extern template bool ArithmeticOperation::MayOverflowWithAddition<char16_t>(const char16_t&, const char16_t&);
+	extern template bool ArithmeticOperation::MayOverflowWithAddition<char32_t>(const char32_t&, const char32_t&);
+	extern template bool ArithmeticOperation::MayOverflowWithSubtraction<bool>(const bool&, const bool&);
+	extern template bool ArithmeticOperation::MayOverflowWithSubtraction<char>(const char&, const char&);
+	extern template bool ArithmeticOperation::MayOverflowWithSubtraction<signed char>(const signed char&, const signed char&);
+	extern template bool ArithmeticOperation::MayOverflowWithSubtraction<unsigned char>(const unsigned char&, const unsigned char&);
+	extern template bool ArithmeticOperation::MayOverflowWithSubtraction<wchar_t>(const wchar_t&, const wchar_t&);
+	extern template bool ArithmeticOperation::MayOverflowWithSubtraction<short>(const short&, const short&);
+	extern template bool ArithmeticOperation::MayOverflowWithSubtraction<unsigned short>(const unsigned short&, const unsigned short&);
+	extern template bool ArithmeticOperation::MayOverflowWithSubtraction<int>(const int&, const int&);
+	extern template bool ArithmeticOperation::MayOverflowWithSubtraction<unsigned int>(const unsigned int&, const unsigned int&);
+	extern template bool ArithmeticOperation::MayOverflowWithSubtraction<long>(const long&, const long&);
+	extern template bool ArithmeticOperation::MayOverflowWithSubtraction<unsigned long>(const unsigned long&, const unsigned long&);
+	extern template bool ArithmeticOperation::MayOverflowWithSubtraction<long long>(const long long&, const long long&);
+	extern template bool ArithmeticOperation::MayOverflowWithSubtraction<unsigned long long>(const unsigned long long&, const unsigned long long&);
+	extern template bool ArithmeticOperation::MayOverflowWithSubtraction<float>(const float&, const float&);
+	extern template bool ArithmeticOperation::MayOverflowWithSubtraction<double>(const double&, const double&);
+	extern template bool ArithmeticOperation::MayOverflowWithSubtraction<long double>(const long double&, const long double&);
+	extern template bool ArithmeticOperation::MayOverflowWithSubtraction<char16_t>(const char16_t&, const char16_t&);
+	extern template bool ArithmeticOperation::MayOverflowWithSubtraction<char32_t>(const char32_t&, const char32_t&);
+	extern template bool ArithmeticOperation::MayOverflowWithMultiplication<bool>(const bool&, const bool&);
+	extern template bool ArithmeticOperation::MayOverflowWithMultiplication<char>(const char&, const char&);
+	extern template bool ArithmeticOperation::MayOverflowWithMultiplication<signed char>(const signed char&, const signed char&);
+	extern template bool ArithmeticOperation::MayOverflowWithMultiplication<unsigned char>(const unsigned char&, const unsigned char&);
+	extern template bool ArithmeticOperation::MayOverflowWithMultiplication<wchar_t>(const wchar_t&, const wchar_t&);
+	extern template bool ArithmeticOperation::MayOverflowWithMultiplication<short>(const short&, const short&);
+	extern template bool ArithmeticOperation::MayOverflowWithMultiplication<unsigned short>(const unsigned short&, const unsigned short&);
+	extern template bool ArithmeticOperation::MayOverflowWithMultiplication<int>(const int&, const int&);
+	extern template bool ArithmeticOperation::MayOverflowWithMultiplication<unsigned int>(const unsigned int&, const unsigned int&);
+	extern template bool ArithmeticOperation::MayOverflowWithMultiplication<long>(const long&, const long&);
+	extern template bool ArithmeticOperation::MayOverflowWithMultiplication<unsigned long>(const unsigned long&, const unsigned long&);
+	extern template bool ArithmeticOperation::MayOverflowWithMultiplication<long long>(const long long&, const long long&);
+	extern template bool ArithmeticOperation::MayOverflowWithMultiplication<unsigned long long>(const unsigned long long&, const unsigned long long&);
+	extern template bool ArithmeticOperation::MayOverflowWithMultiplication<float>(const float&, const float&);
+	extern template bool ArithmeticOperation::MayOverflowWithMultiplication<double>(const double&, const double&);
+	extern template bool ArithmeticOperation::MayOverflowWithMultiplication<long double>(const long double&, const long double&);
+	extern template bool ArithmeticOperation::MayOverflowWithMultiplication<char16_t>(const char16_t&, const char16_t&);
+	extern template bool ArithmeticOperation::MayOverflowWithMultiplication<char32_t>(const char32_t&, const char32_t&);
+	extern template bool ArithmeticOperation::MayOverflowWithDivision<bool>(const bool&, const bool&);
+	extern template bool ArithmeticOperation::MayOverflowWithDivision<char>(const char&, const char&);
+	extern template bool ArithmeticOperation::MayOverflowWithDivision<signed char>(const signed char&, const signed char&);
+	extern template bool ArithmeticOperation::MayOverflowWithDivision<unsigned char>(const unsigned char&, const unsigned char&);
+	extern template bool ArithmeticOperation::MayOverflowWithDivision<wchar_t>(const wchar_t&, const wchar_t&);
+	extern template bool ArithmeticOperation::MayOverflowWithDivision<short>(const short&, const short&);
+	extern template bool ArithmeticOperation::MayOverflowWithDivision<unsigned short>(const unsigned short&, const unsigned short&);
+	extern template bool ArithmeticOperation::MayOverflowWithDivision<int>(const int&, const int&);
+	extern template bool ArithmeticOperation::MayOverflowWithDivision<unsigned int>(const unsigned int&, const unsigned int&);
+	extern template bool ArithmeticOperation::MayOverflowWithDivision<long>(const long&, const long&);
+	extern template bool ArithmeticOperation::MayOverflowWithDivision<unsigned long>(const unsigned long&, const unsigned long&);
+	extern template bool ArithmeticOperation::MayOverflowWithDivision<long long>(const long long&, const long long&);
+	extern template bool ArithmeticOperation::MayOverflowWithDivision<unsigned long long>(const unsigned long long&, const unsigned long long&);
+	extern template bool ArithmeticOperation::MayOverflowWithDivision<float>(const float&, const float&);
+	extern template bool ArithmeticOperation::MayOverflowWithDivision<double>(const double&, const double&);
+	extern template bool ArithmeticOperation::MayOverflowWithDivision<long double>(const long double&, const long double&);
+	extern template bool ArithmeticOperation::MayOverflowWithDivision<char16_t>(const char16_t&, const char16_t&);
+	extern template bool ArithmeticOperation::MayOverflowWithDivision<char32_t>(const char32_t&, const char32_t&);
 }
 #endif // __stationaryorbit_core_arithmetic__
