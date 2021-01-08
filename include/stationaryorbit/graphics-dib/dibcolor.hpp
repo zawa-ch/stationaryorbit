24c406a88ac44c7cb0a947ad5db582d63f45ed44
zawa-ch <satellite.2e1834097@gmail.com>
2021-01-08 20:52:27 +0900

    + DIBPixelPerser

--- include/stationaryorbit/graphics-dib/dibcolor.hpp
+++ include/stationaryorbit/graphics-dib/dibcolor.hpp
@@ -48,8 +48,8 @@ namespace zawa_ch::StationaryOrbit::Graphics::DIB
 		)
 		{}
 
-		constexpr const DataType& Data() const { return _data; }
-		constexpr ValueType Value() const { return _data.Value(); }
+		[[nodiscard]] constexpr const DataType& Data() const { return _data; }
+		[[nodiscard]] constexpr ValueType Value() const { return _data.Value(); }
 
 		constexpr explicit operator uint32_t() const
 		{
@@ -65,5 +65,44 @@ namespace zawa_ch::StationaryOrbit::Graphics::DIB
 	static_assert(sizeof(DIBPixelData<BitDepth::Bit16>) == 2, "");
 	static_assert(sizeof(DIBPixelData<BitDepth::Bit24>) == 3, "");
 	static_assert(sizeof(DIBPixelData<BitDepth::Bit32>) == 4, "");
+
+	class DIBPixelPerser
+	{
+		DIBPixelPerser() = delete;
+		DIBPixelPerser(const DIBPixelPerser&) = delete;
+		DIBPixelPerser(DIBPixelPerser&&) = delete;
+		~DIBPixelPerser() = delete;
+	public:
+		[[nodiscard]] static constexpr GrayScale8_t ToGrayScale(const DIBPixelData<BitDepth::Bit1>& data) { return GrayScale8_t(Proportion8_t(uint32_t(data), 1)); }
+		[[nodiscard]] static constexpr GrayScale8_t ToGrayScale(const DIBPixelData<BitDepth::Bit4>& data) { return GrayScale8_t(Proportion8_t(uint32_t(data), 15)); }
+		[[nodiscard]] static constexpr GrayScale8_t ToGrayScale(const DIBPixelData<BitDepth::Bit8>& data) { return GrayScale8_t(Proportion8_t(uint32_t(data), 255)); }
+		[[nodiscard]] static constexpr RGB8_t ToRGB(const DIBPixelData<BitDepth::Bit16>& data)
+		{
+			return RGB8_t
+			(
+				Proportion8_t((uint32_t(data) >> 10) & 0x1F, 0x1F),
+				Proportion8_t((uint32_t(data) >> 5) & 0x1F, 0x1F),
+				Proportion8_t(uint32_t(data) & 0x1F, 0x1F)
+			);
+		}
+		[[nodiscard]] static constexpr RGB8_t ToRGB(const DIBPixelData<BitDepth::Bit24>& data)
+		{
+			return RGB8_t
+			(
+				Proportion8_t((uint32_t(data) >> 16) & 0xFF, 0xFF),
+				Proportion8_t((uint32_t(data) >> 8) & 0xFF, 0xFF),
+				Proportion8_t(uint32_t(data) & 0xFF, 0xFF)
+			);
+		}
+		[[nodiscard]] static constexpr RGB8_t ToRGB(const DIBPixelData<BitDepth::Bit32>& data)
+		{
+			return RGB8_t
+			(
+				Proportion8_t((uint32_t(data) >> 16) & 0xFF, 0xFF),
+				Proportion8_t((uint32_t(data) >> 8) & 0xFF, 0xFF),
+				Proportion8_t(uint32_t(data) & 0xFF, 0xFF)
+			);
+		}
+	};
 }
 #endif // __stationaryorbit_graphics_dib_dibcolor__
