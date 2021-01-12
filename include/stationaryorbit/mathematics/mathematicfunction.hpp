c10d1b4dcdf450286ab9b14482656849547ff4a7
zawa-ch <satellite.2e1834097@gmail.com>
2021-01-12 19:17:35 +0900

    * Mathematics::MathematicFunction

--- include/stationaryorbit/mathematics/mathematicfunction.hpp
+++ include/stationaryorbit/mathematics/mathematicfunction.hpp
@@ -18,27 +18,30 @@
 //
 #ifndef __stationaryorbit_mathematics_mathematicfunction__
 #define __stationaryorbit_mathematics_mathematicfunction__
+#include "stationaryorbit/core.traits.hpp"
 #include <functional>
 namespace zawa_ch::StationaryOrbit::Mathematics
 {
 
-    /// @a T 型の値を受け取り、 @a T 型の値を返す数学的な関数を実装します。
-    template<class T>
-    class IMathematicFunction
-    {
-    public:
-        /// この関数に値を代入した解を取得します。
-        /// @param  value
-        /// 代入する値。
-        /// @return
-        /// 計算を行い、得られた解が返ります。
-        virtual T Calc(const T& value) const = 0;
-    };
-    /// @a T 型の値を受け取り、 @a T 型の値を返す数学的な関数を表します。
+	/// @a T 型の値を受け取り、 @a T 型の値を返す数学的な関数を実装します。
+	template<class T>
+	class IMathematicFunction
+	{
+		static_assert(Traits::IsNumericalType<T>, "テンプレート型 T は数値型である必要があります。");
+	public:
+		/// この関数に値を代入した解を取得します。
+		/// @param  value
+		/// 代入する値。
+		/// @return
+		/// 計算を行い、得られた解が返ります。
+		virtual T Calc(const T& value) const = 0;
+	};
+	/// @a T 型の値を受け取り、 @a T 型の値を返す数学的な関数を表します。
 	template<class T>
 	class MathematicFunction
 		: public IMathematicFunction<T>
 	{
+		static_assert(Traits::IsNumericalType<T>, "テンプレート型 T は数値型である必要があります。");
 	public:
 		///	@a T 型の値を受け取り、 @a T 型の値を返す数学的な関数の実体。
 		typedef std::function<T(T)> HandlerType;
@@ -47,11 +50,11 @@ namespace zawa_ch::StationaryOrbit::Mathematics
 	public:
 		///	実体となる関数を指定してこのオブジェクトを初期化します。
 		explicit MathematicFunction(HandlerType function) : func(function) {}
-        /// この関数に値を代入した解を取得します。
-        /// @param  value
-        /// 代入する値。
-        /// @return
-        /// 計算を行い、得られた解が返ります。
+		/// この関数に値を代入した解を取得します。
+		/// @param  value
+		/// 代入する値。
+		/// @return
+		/// 計算を行い、得られた解が返ります。
 		T Calc(const T& value) const { return func(value); }
 		T operator()(const T& value) const { return Calc(value); }
 	};
