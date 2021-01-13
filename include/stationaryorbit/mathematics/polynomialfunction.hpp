64a6413ad61861d0f7bedc252667f4279c2a6d84
zawa-ch <satellite.2e1834097@gmail.com>
2021-01-13 16:41:52 +0900

    * Mathematics::PolynomialFunction

--- include/stationaryorbit/mathematics/polynomialfunction.hpp
+++ include/stationaryorbit/mathematics/polynomialfunction.hpp
@@ -18,45 +18,61 @@
 //
 #ifndef __stationaryorbit_mathematics_polynomialfunction__
 #define __stationaryorbit_mathematics_polynomialfunction__
-#include <vector>
+#include <array>
 #include "stationaryorbit/core.exception.hpp"
+#include "stationaryorbit/core.numeral.hpp"
 #include "mathematicfunction.hpp"
 namespace zawa_ch::StationaryOrbit::Mathematics
 {
-
 	///	実数の多項式を表します。
-	class PolynomialFunction
-		: public IMathematicFunction<double>
+	template<typename T, size_t N>
+	class PolynomialFunction : public IMathematicFunction<T>
 	{
-	private:
-
-		std::vector<double> num;
-
+		static_assert(Traits::IsNumericalType<T>, "テンプレート型 T は数値型である必要があります。");
 	public:
-
+		typedef T ValueType;
+		typedef std::array<T, N> DataType;
+	private:
+		DataType num;
+	public:
 		///	空のオブジェクトを作成します。
 		PolynomialFunction() = default;
-
 		///	多項式のオブジェクトを初期化します。
 		///	x^n * num[n-1] + ... x^2 * num[2] + x * num[1] + num[0] の順で計算されます。
-		explicit PolynomialFunction(const std::vector<double>& value);
-
+		explicit PolynomialFunction(const DataType& value) : num(value) {}
 		///	多項式への参照を取得します。
-		std::vector<double>& Polynomial() { return num; }
-
-		///	多項式を取得します。
-		std::vector<double> getPolynomial() const { return num; }
-
-        /// この関数に値を代入した解を取得します。
-        ///
-        /// @param  value
-        /// 代入する値。
-        ///
-        /// @return
-        /// 計算を行い、得られた解が返ります。
-		double Calc(const double& value) const;
-		double operator()(const double& value) const { return Calc(value); }
-
+		const DataType& Polynomial() const { return num; }
+		/// この関数に値を代入した解を取得します。
+		/// @param  value
+		/// 代入する値。
+		/// @return
+		/// 計算を行い、得られた解が返ります。
+		ValueType Calc(const ValueType& value) const
+		{
+			if constexpr (std::numeric_limits<T>::is_exact)
+			{
+				auto numvalue = T();
+				for(auto i: Range<size_t>(0, num.size()).GetStdReverseIterator())
+				{
+					numvalue += num[i];
+					numvalue *= value;
+				}
+				numvalue += num[0];
+				return numvalue;
+			}
+			else
+			{
+				auto numvalue = CompensatedFloat<T>();
+				for(auto i: Range<size_t>(0, num.size()).GetStdReverseIterator())
+				{
+					numvalue += num[i];
+					numvalue *= value;
+				}
+				numvalue += num[0];
+				return T(numvalue);
+			}
+		}
+		ValueType operator()(const ValueType& value) const { return Calc(value); }
 	};
 
 }
--- src/mathematics/polynomialfunction.cpp
+++ src/mathematics/polynomialfunction.cpp
@@ -19,20 +19,3 @@
 #include "stationaryorbit/mathematics/compensatedfloat.hpp"
 #include "stationaryorbit/mathematics/polynomialfunction.hpp"
 using namespace zawa_ch::StationaryOrbit::Mathematics;
-
-PolynomialFunction::PolynomialFunction(const std::vector<double>& value)
-	: num(value)
-{}
-
-double PolynomialFunction::Calc(const double& value) const
-{
-	if (num.empty()) throw InvalidOperationException("");
-	CompensatedDouble numvalue = CompensatedDouble();
-	for(size_t i = num.size() - 1; 0 < i; i--)
-	{
-		numvalue += num[i];
-		numvalue *= value;
-	}
-	numvalue += num[0];
-	return double(numvalue);
-}
