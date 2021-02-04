4da86e639049eb21f1d6087f55ad69fbb303bbf6
zawa-ch <satellite.2e1834097@gmail.com>
2021-02-04 16:22:35 +0900

    * Graphics::DIB::DIBBitmapRGBDecoder ヘッダ独立

--- include/stationaryorbit/graphics-dib/dibinfobitmap.hpp
+++ include/stationaryorbit/graphics-dib/dibinfobitmap.hpp
@@ -21,118 +21,10 @@
 #include <vector>
 #include <functional>
 #include <optional>
-#include <variant>
 #include "dibloader.hpp"
+#include "rgbdecoder.hpp"
 namespace zawa_ch::StationaryOrbit::Graphics::DIB
 {
-	class DIBBitmapRGBDecoder
-	{
-	public:
-		typedef std::variant<DIBPixelData<DIBBitDepth::Bit1>, DIBPixelData<DIBBitDepth::Bit4>, DIBPixelData<DIBBitDepth::Bit8>, DIBPixelData<DIBBitDepth::Bit16>, DIBPixelData<DIBBitDepth::Bit24>, DIBPixelData<DIBBitDepth::Bit32>> ValueType;
-	private:
-		///	デコード時に読み込みを行う @a DIBLoader への参照。
-		DIBLoader& loader;
-		///	読み込み先のデータのオフセット。
-		size_t offset;
-		///	画像の大きさ。
-		DisplayRectSize size;
-		///	全体の要素数。
-		int64_t length;
-		///	現在の読み込み位置。
-		int64_t current;
-		///	現在の値。
-		ValueType current_value;
-		///	各ピクセルのデータ長。
-		DIBBitDepth bitdepth;
-		///	1ピクセルのデータ長(バイト単位)。
-		const size_t pixellength;
-		///	ストライド(1水平軸ラインのデータ長)。
-		const size_t stridelength;
-	public:
-		DIBBitmapRGBDecoder(DIBLoader& loader, size_t offset, DIBBitDepth bitdepth, const DisplayRectSize& size);
-		virtual ~DIBBitmapRGBDecoder() = default;
-
-		///	現在の位置を次に進めます。
-		bool Next();
-		///	現在の位置を指定された数だけ次に進めます。
-		bool Next(const IteratorTraits::IteratorDiff_t& count);
-		///	現在の位置を前に戻します。
-		bool Previous();
-		///	現在の位置を指定された数だけ前に戻します。
-		bool Previous(const IteratorTraits::IteratorDiff_t& count);
-		///	画像の任意の位置にジャンプします。
-		void JumpTo(const DisplayPoint& pos);
-		///	現在の位置を初期位置に戻します。
-		void Reset();
-		///	現在の位置を初期位置に戻します。
-		void Reset(const IteratorOrigin& origin);
-		///	現在の位置が値を持っているかを取得します。
-		[[nodiscard]] bool HasValue() const;
-		///	現在の位置が最初の要素よりも前にいるかを取得します。
-		[[nodiscard]] bool IsBeforeBegin() const;
-		///	現在の位置が最後の要素よりも後にいるかを取得します。
-		[[nodiscard]] bool IsAfterEnd() const;
-		///	現在の位置を画像上での位置で取得します。
-		[[nodiscard]] DisplayPoint CurrentPos() const;
-		///	現在の位置にあるオブジェクトを取得します。
-		[[nodiscard]] ValueType Current() const;
-		///	現在の位置にオブジェクトを書き込みます。
-		void Write(const ValueType& value);
-		///	指定されたオブジェクトとの距離を取得します。
-		[[nodiscard]] IteratorTraits::IteratorDiff_t Distance(const DIBBitmapRGBDecoder& other) const;
-		[[nodiscard]] bool Equals(const DIBBitmapRGBDecoder& other) const;
-		[[nodiscard]] int Compare(const DIBBitmapRGBDecoder& other) const;
-	private:
-		[[nodiscard]] ValueType Get(size_t index);
-		[[nodiscard]] size_t ResolveIndex(const DisplayPoint& pos) const;
-		[[nodiscard]] DisplayPoint ResolvePos(size_t index) const;
-		[[nodiscard]] size_t ResolveOffset(const DisplayPoint& pos) const;
-		[[nodiscard]] size_t ResolveOffset(size_t index) const;
-	};
-	class DIBBitmapRGBEncoder
-	{
-	public:
-		typedef std::variant<DIBPixelData<DIBBitDepth::Bit1>, DIBPixelData<DIBBitDepth::Bit4>, DIBPixelData<DIBBitDepth::Bit8>, DIBPixelData<DIBBitDepth::Bit16>, DIBPixelData<DIBBitDepth::Bit24>, DIBPixelData<DIBBitDepth::Bit32>> ValueType;
-	private:
-		///	書き込みを行う @a DIBLoader への参照。
-		DIBLoader& loader;
-		///	書き込み先のデータのオフセット。
-		size_t offset;
-		///	画像の大きさ。
-		DisplayRectSize size;
-		///	全体の要素数。
-		int64_t length;
-		///	現在の書き込み位置。
-		int64_t current;
-		///	各ピクセルのデータ長。
-		DIBBitDepth bitdepth;
-		///	1ピクセルのデータ長(バイト単位)。
-		const size_t pixellength;
-		///	ストライド(1水平軸ラインのデータ長)。
-		const size_t stridelength;
-	public:
-		DIBBitmapRGBEncoder(DIBLoader& loader, size_t offset, DIBBitDepth bitdepth, const DisplayRectSize& size);
-		virtual ~DIBBitmapRGBEncoder() = default;
-
-		///	現在の位置にオブジェクトを書き込み、現在の位置を次に進めます。
-		void Write(const ValueType& value);
-		///	現在の位置を初期位置に戻します。
-		void Reset();
-		///	現在の位置が値を持っているかを取得します。
-		[[nodiscard]] bool HasValue() const;
-		///	現在の位置を画像上での位置で取得します。
-		[[nodiscard]] DisplayPoint CurrentPos() const;
-		[[nodiscard]] bool Equals(const DIBBitmapRGBEncoder& other) const;
-		[[nodiscard]] int Compare(const DIBBitmapRGBEncoder& other) const;
-
-		[[nodiscard]] static size_t GetPxLength(DIBBitDepth bitdepth);
-		[[nodiscard]] static size_t GetStrideLength(DIBBitDepth bitdepth, const DisplayRectSize& size);
-		[[nodiscard]] static size_t GetImageLength(DIBBitDepth bitdepth, const DisplayRectSize& size);
-	private:
-		[[nodiscard]] size_t ResolveIndex(const DisplayPoint& pos) const;
-		[[nodiscard]] DisplayPoint ResolvePos(size_t index) const;
-		[[nodiscard]] size_t ResolveOffset(const DisplayPoint& pos) const;
-	};
 	///	@a DIBLoader を使用してInfoHeaderを持つWindows bitmap 画像を読み込みます。
 	class DIBInfoBitmap
 	{
@@ -226,7 +118,25 @@ namespace zawa_ch::StationaryOrbit::Graphics::DIB
 		///	画像の切り抜き範囲。
 		[[nodiscard]] Pixmap ToPixmap(const DisplayRectangle& area);
 
+		///	指定された @a DIBLoader に画像データを書き込み、 @a DIBInfoBitmap を生成します。
+		///	@param	loader
+		///	書き込み先の @a DIBLoader オブジェクト。
+		///	このオブジェクトで「消費」されるため、右辺値参照である必要があります。
+		///	@param	header
+		///	生成時に格納する画像ヘッダデータ。
+		///	@param	image
+		///	生成時に格納する画像データ。
 		static std::optional<DIBInfoBitmap> Generate(DIBLoader&& loader, const DIBInfoHeader& header, const Image<RGB8_t>& image);
+		///	指定された @a DIBLoader に画像データを書き込み、 @a DIBInfoBitmap を生成します。
+		///	@param	loader
+		///	書き込み先の @a DIBLoader オブジェクト。
+		///	このオブジェクトで「消費」されるため、右辺値参照である必要があります。
+		///	@param	header
+		///	生成時に格納する画像ヘッダデータ。
+		///	@param	palette
+		///	生成時に格納する色パレット。
+		///	@param	image
+		///	生成時に格納する画像データ。
 		static std::optional<DIBInfoBitmap> Generate(DIBLoader&& loader, const DIBInfoHeader& header, const std::vector<RGB8_t> palette, const Image<RGB8_t>& image);
 	private:
 		[[nodiscard]] ValueType ConvertToRGB(const DIBBitmapRGBDecoder::ValueType& data) const;
