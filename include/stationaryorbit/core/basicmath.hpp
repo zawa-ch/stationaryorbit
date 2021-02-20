6a06066397479cfbd991f2e2748bfcf826bd5523
zawa-ch <satellite.2e1834097@gmail.com>
2021-02-20 20:47:53 +0900

    * BasicMathematics sqrt実装修正

--- include/stationaryorbit/core/basicmath.hpp
+++ include/stationaryorbit/core/basicmath.hpp
@@ -31,6 +31,15 @@ namespace zawa_ch::StationaryOrbit
 		BasicMathematics(const BasicMathematics&) = delete;
 		BasicMathematics(BasicMathematics&&) = delete;
 		~BasicMathematics() = delete;
+
+		template<class T, class R = decltype( std::sqrt(std::declval<T&>()) )> struct StdSqrtResult_Impl { typedef R type; };
+		template<class T, class R = decltype( std::declval<const T&>().Sqrt() )> struct SqrtResult_Impl { typedef R type; };
+		template<class T, class = void> struct IsStdSqrtCallable_Impl : std::false_type {};
+		template<class T> struct IsStdSqrtCallable_Impl<T, std::void_t< typename StdSqrtResult_Impl<T>::type >> : std::true_type {};
+		template<class T, class = void> struct HasSqrt_Impl : std::false_type {};
+		template<class T> struct HasSqrt_Impl<T, std::void_t< typename StdSqrtResult_Impl<T>::type >> : std::true_type {};
+		template<class T> static constexpr bool IsStdSqrtCallable = IsStdSqrtCallable_Impl<T>::value;
+		template<class T> static constexpr bool HasSqrt = HasSqrt_Impl<T>::value;
 	public:
 		template<class T>
 		[[nodiscard]] static constexpr std::enable_if_t<Traits::IsNumericalType<T>, T> Abstract(const T& value) noexcept
@@ -40,15 +49,16 @@ namespace zawa_ch::StationaryOrbit
 		template<class T, class = decltype( std::abs( std::declval<T&>() ))>
 		[[nodiscard]] static constexpr T Abstract(const T& value) noexcept { return std::abs(value); }
 		///	この値の平方根を取得します。
-		template<class T>
-		[[nodiscard]] static constexpr std::enable_if_t<Traits::IsNumericalType<T>, T> Sqrt(const T& value) noexcept
+		template<class T, std::enable_if_t<(!IsStdSqrtCallable<T>) && (!HasSqrt<T>), int> = 0>
+		[[nodiscard]] static constexpr T Sqrt(const T& value) noexcept
 		{
+			static_assert(Traits::IsNumericalType<T>, "テンプレート型 T は数値型である必要があります。");
 			auto result = value;
 			auto b = value;
 			do	// X[n+1] = (X[N] + a / X[N]) / 2
 			{
 				// 0除算の回避(sqrt(0) = 0)
-				if (result == T(0U))
+				if (result == T(0))
 				{
 					if constexpr (std::numeric_limits<T>::has_quiet_NaN) { result = std::numeric_limits<T>::quiet_NaN(); }
 					break;
@@ -57,16 +67,21 @@ namespace zawa_ch::StationaryOrbit
 				// a / X[N] の導出
 				auto delta = value / b;
 				// X[N] / 2
-				result /= 2;
-				// 剰余分の計算
-				result += ((result % 2) + (delta % 2)) / 2;
+				result /= T(2);
+				if constexpr (Traits::IsIntegralType<T>)
+				{
+					// 剰余分の計算
+					result += ((result % T(2)) + (delta % T(2))) / T(2);
+				}
 				// (a / X[n]) / 2
-				result += delta / 2;
-			} while (2U < ((result < b)?(b-result):(result - b)));
+				result += delta / T(2);
+			} while (T(2) < ((result < b)?(b-result):(result - b)));
 			return result;
 		}
-		template<class T, class = decltype( std::sqrt( std::declval<T&>() ))>
+		template<class T, std::enable_if_t<(IsStdSqrtCallable<T>), int> = 0>
 		[[nodiscard]] static constexpr T Sqrt(const T& value) noexcept { return std::sqrt(value); }
+		template<class T, std::enable_if_t<(!IsStdSqrtCallable<T>) && (HasSqrt<T>), int> = 0>
+		[[nodiscard]] static constexpr decltype( std::declval<const T&>().Sqrt() ) Sqrt(const T& value) noexcept { return value.Sqrt(); }
 		template<class T>
 		[[nodiscard]] static constexpr std::enable_if_t<Traits::IsNumericalType<T>, T> Mod(const T& left, const T& right)
 		{
